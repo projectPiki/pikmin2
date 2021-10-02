7fa0c0246b1dc4394ad7dcd8c0bcf5f8c462a0d2
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-10-02 13:10:57 -0400

    Delete PSCreatureMgr.s

--- asm/plugProjectHikinoU/PSCreatureMgr.s
+++
@@ -1,6 +0,0 @@
-.include "macros.inc"
-
-.section .text, "ax"  # 0x800056C0 - 0x80472F00
-.global registerAnime__Q26PSGame16SoundCreatureMgrFP10J3DAnmBasePCc
-registerAnime__Q26PSGame16SoundCreatureMgrFP10J3DAnmBasePCc:
-/* 8033EE98 0033BDD8  4E 80 00 20 */	blr 
