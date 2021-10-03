fe7ea15827d81fe61750021c3988488473c74f12
intns <84647527+intns@users.noreply.github.com>
2021-10-03 17:32:16 +0100

    Revert "Start eggAnimator"
    This reverts commit bee365d6548194c5eb99bac093f09b1db9e91d41.

--- src/plugProjectMorimuraU/eggAnimator.cpp
+++ src/plugProjectMorimuraU/eggAnimator.cpp
@@ -1,13 +1,27 @@
-#include "eggAnimator.h"
+
+
+/*
+ * --INFO--
+ * Address:	........
+ * Size:	0000E4
+ */
+void _Print(char*, ...)
+{
+    // UNUSED FUNCTION
+}
 
 /*
  * --INFO--
  * Address:	8034B63C
  * Size:	000008
  */
-void Game::Egg::ProperAnimator::setAnimMgr(SysShape::AnimMgr* mgr)
+void Game::Egg::ProperAnimator::setAnimMgr((SysShape::AnimMgr*))
 {
-    m_manager = mgr;
+    /*
+    .loc_0x0:
+      stw       r4, 0x20(r3)
+      blr
+    */
 }
 
 /*
@@ -15,9 +29,13 @@ void Game::Egg::ProperAnimator::setAnimMgr(SysShape::AnimMgr* mgr)
  * Address:	8034B644
  * Size:	000008
  */
-u32* Game::Egg::ProperAnimator::getAnimator(int animatorNum)
+void Game::Egg::ProperAnimator::getAnimator((int))
 {
-    return &m_animator;
+    /*
+    .loc_0x0:
+      addi      r3, r3, 0x10
+      blr
+    */
 }
 
 /*
@@ -25,43 +43,50 @@ u32* Game::Egg::ProperAnimator::getAnimator(int animatorNum)
  * Address:	8034B64C
  * Size:	00005C
  */
-// Game::Egg::ProperAnimator::~ProperAnimator()
-// {
-//     /*
-//     .loc_0x0:
-//       stwu      r1, -0x10(r1)
-//       mflr      r0
-//       stw       r0, 0x14(r1)
-//       stw       r31, 0xC(r1)
-//       mr.       r31, r3
-//       beq-      .loc_0x44
-//       lis       r3, 0x804E
-//       subi      r0, r3, 0x40A0
-//       stw       r0, 0x0(r31)
-//       beq-      .loc_0x34
-//       lis       r3, 0x804B
-//       subi      r0, r3, 0x23A8
-//       stw       r0, 0x0(r31)
+void Game::Egg::ProperAnimator::__dt(void)
+{
+    /*
+    .loc_0x0:
+      stwu      r1, -0x10(r1)
+      mflr      r0
+      stw       r0, 0x14(r1)
+      stw       r31, 0xC(r1)
+      mr.       r31, r3
+      beq-      .loc_0x44
+      lis       r3, 0x804E
+      subi      r0, r3, 0x40A0
+      stw       r0, 0x0(r31)
+      beq-      .loc_0x34
+      lis       r3, 0x804B
+      subi      r0, r3, 0x23A8
+      stw       r0, 0x0(r31)
 
-//     .loc_0x34:
-//       extsh.    r0, r4
-//       ble-      .loc_0x44
-//       mr        r3, r31
-//       bl        -0x3275D8
+    .loc_0x34:
+      extsh.    r0, r4
+      ble-      .loc_0x44
+      mr        r3, r31
+      bl        -0x3275D8
 
-//     .loc_0x44:
-//       lwz       r0, 0x14(r1)
-//       mr        r3, r31
-//       lwz       r31, 0xC(r1)
-//       mtlr      r0
-//       addi      r1, r1, 0x10
-//       blr
-//     */
-// }
+    .loc_0x44:
+      lwz       r0, 0x14(r1)
+      mr        r3, r31
+      lwz       r31, 0xC(r1)
+      mtlr      r0
+      addi      r1, r1, 0x10
+      blr
+    */
+}
 
-// /*
-//  * --INFO--
-//  * Address:	8034B6A8
-//  * Size:	000008
-//  */
-// u32* Game::Egg::ProperAnimator::getAnimator(void) { return &m_animator; }
+/*
+ * --INFO--
+ * Address:	8034B6A8
+ * Size:	000008
+ */
+void Game::Egg::ProperAnimator::getAnimator(void)
+{
+    /*
+    .loc_0x0:
+      addi      r3, r3, 0x10
+      blr
+    */
+}
