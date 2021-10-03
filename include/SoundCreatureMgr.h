54aabeba4c677f754dbb5e15d3add8b05847f56d
intns <84647527+intns@users.noreply.github.com>
2021-10-03 06:00:25 +0100

    Cleaned up PSCreatureMgr

--- include/SoundCreatureMgr.h
+++ include/SoundCreatureMgr.h
@@ -1,44 +1,37 @@
-#ifndef SoundCreatureMgr_h
-#define SoundCreatureMgr_h
-
-typedef struct J3DAnmBase__vt J3DAnmBase__vt, *PJ3DAnmBase__vt;
+#ifndef _SOUNDCREATUREMGR_H
+#define _SOUNDCREATUREMGR_H
 
 typedef enum J3DAnmKind {
-    CLUSTER=3,
-    CLUSTER_FULL=12,
-    CLUSTER_KEY=13,
-    COLOR=1,
-    COLOR_FULL=10,
-    COLOR_KEY=11,
-    TEV_REG_KEY=5,
-    TEXTURE_SRT_KEY=4,
-    TEX_PATTERN=2,
-    TRANSFORM=0,
-    TRANSFORM_FULL=9,
-    TRANSFORM_KEY=8,
-    VISIBILITY_FULL=6,
-    VTX_COLOR=7,
-    VTX_COLOR_FULL=14,
-    VTX_COLOR_KEY=15
+    CLUSTER         = 3,
+    CLUSTER_FULL    = 12,
+    CLUSTER_KEY     = 13,
+    COLOR           = 1,
+    COLOR_FULL      = 10,
+    COLOR_KEY       = 11,
+    TEV_REG_KEY     = 5,
+    TEXTURE_SRT_KEY = 4,
+    TEX_PATTERN     = 2,
+    TRANSFORM       = 0,
+    TRANSFORM_FULL  = 9,
+    TRANSFORM_KEY   = 8,
+    VISIBILITY_FULL = 6,
+    VTX_COLOR       = 7,
+    VTX_COLOR_FULL  = 14,
+    VTX_COLOR_KEY   = 15
 };
 
-struct J3DAnmBase__vt {
-    unsigned int* field_0x0;
-    unsigned int* field_0x4;
-    void (* dtor)(void *, short);
-    J3DAnmKind (* getKind)(void);
-};
-
-typedef struct J3DAnmBase J3DAnmBase, *PJ3DAnmBase;
 struct J3DAnmBase { /* PlaceHolder Class Structure */
-    struct J3DAnmBase__vt * __vt;
+    virtual void unk00() = 0;
+    virtual void unk04() = 0;
+    virtual ~J3DAnmBase();
+    virtual J3DAnmKind getKind();
 };
 
 namespace PSGame {
-  class SoundCreatureMgr {
-  public:
-	void registerAnime(J3DAnmBase*, const char*);
-  };
-}
+class SoundCreatureMgr {
+public:
+    void registerAnime(J3DAnmBase*, const char*);
+};
+} // namespace PSGame
 
 #endif
--- src/plugProjectHikinoU/PSCreatureMgr.cpp
+++ src/plugProjectHikinoU/PSCreatureMgr.cpp
@@ -1,30 +1,10 @@
+#include "SoundCreatureMgr.h"
 
+namespace PSGame {
 
-/*
- * --INFO--
- * Address:	........
- * Size:	000004
- */
-* /
-    //{
-    // UNUSED FUNCTION
-    //}
-
-    /*
-     * --INFO--
-     * Address:	8033EE98
-     * Size:	000004
-     */
-    * /
-
-    namespace PSGame
+void SoundCreatureMgr::registerAnime(J3DAnmBase* arg1, const char* arg2)
 {
-    void SoundCreatureMgr::registerAnime(J3DAnmBase * arg1, const char* arg2)
-    {
-        return;
-    }
-    /*
-    .loc_0x0:
-      blr
-    */
+    return;
 }
+
+} // namespace PSGame
