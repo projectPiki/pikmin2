dda0c5bc683208c612f65aeaf5f8f14135683a21
intns <84647527+intns@users.noreply.github.com>
2021-10-07 14:38:51 +0100

    BitFlags dump and read

--- include/BitFlags.h
+++ include/BitFlags.h
@@ -1,14 +1,17 @@
 #ifndef _BITFLAGS_H
 #define _BITFLAGS_H
 
+#include "Stream.h"
 #include "types.h"
 
 struct BitFlags {
-    u32 _00; // _00
-    u16 _04; // _04
-    u16 _06; // _06
+    u8* m_flagArr; // _00 flag array
+    u16 _04;       // _04
+    u16 m_flagCnt; // _06 flag count
 
     BitFlags();
+    void dump();
+    void read(Stream&);
 };
 
 #endif
