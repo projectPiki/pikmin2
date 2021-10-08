9766f52cd2d67c663e14184339b2cfa6b3773e89
intns <84647527+intns@users.noreply.github.com>
2021-10-08 01:31:19 +0100

    Game::MemoryCard::Resource::vtbl is multiply defined

--- asm/sysGCU/pikmin2MemoryCardMgr.s
+++ asm/sysGCU/pikmin2MemoryCardMgr.s
@@ -2,36 +2,6 @@
 
 .section .text, "ax"  # 0x800056C0 - 0x80472F00
 
-.global __dt__Q34Game10MemoryCard8ResourceFv
-__dt__Q34Game10MemoryCard8ResourceFv:
-/* 80442AB4 0043F9F4  94 21 FF F0 */	stwu r1, -0x10(r1)
-/* 80442AB8 0043F9F8  7C 08 02 A6 */	mflr r0
-/* 80442ABC 0043F9FC  90 01 00 14 */	stw r0, 0x14(r1)
-/* 80442AC0 0043FA00  93 E1 00 0C */	stw r31, 0xc(r1)
-/* 80442AC4 0043FA04  7C 9F 23 78 */	mr r31, r4
-/* 80442AC8 0043FA08  93 C1 00 08 */	stw r30, 8(r1)
-/* 80442ACC 0043FA0C  7C 7E 1B 79 */	or. r30, r3, r3
-/* 80442AD0 0043FA10  41 82 00 34 */	beq lbl_80442B04
-/* 80442AD4 0043FA14  3C 60 80 4F */	lis r3, __vt__Q34Game10MemoryCard8Resource@ha
-/* 80442AD8 0043FA18  38 03 CF D8 */	addi r0, r3, __vt__Q34Game10MemoryCard8Resource@l
-/* 80442ADC 0043FA1C  90 1E 00 00 */	stw r0, 0(r30)
-/* 80442AE0 0043FA20  80 7E 00 18 */	lwz r3, 0x18(r30)
-/* 80442AE4 0043FA24  48 00 02 09 */	bl destroyResource__Q34Game10MemoryCard3MgrFv
-/* 80442AE8 0043FA28  7F C3 F3 78 */	mr r3, r30
-/* 80442AEC 0043FA2C  38 80 00 00 */	li r4, 0
-/* 80442AF0 0043FA30  4B BD A5 45 */	bl __dt__11JKRDisposerFv
-/* 80442AF4 0043FA34  7F E0 07 35 */	extsh. r0, r31
-/* 80442AF8 0043FA38  40 81 00 0C */	ble lbl_80442B04
-/* 80442AFC 0043FA3C  7F C3 F3 78 */	mr r3, r30
-/* 80442B00 0043FA40  4B BE 15 B5 */	bl __dl__FPv
-lbl_80442B04:
-/* 80442B04 0043FA44  80 01 00 14 */	lwz r0, 0x14(r1)
-/* 80442B08 0043FA48  7F C3 F3 78 */	mr r3, r30
-/* 80442B0C 0043FA4C  83 E1 00 0C */	lwz r31, 0xc(r1)
-/* 80442B10 0043FA50  83 C1 00 08 */	lwz r30, 8(r1)
-/* 80442B14 0043FA54  7C 08 03 A6 */	mtlr r0
-/* 80442B18 0043FA58  38 21 00 10 */	addi r1, r1, 0x10
-/* 80442B1C 0043FA5C  4E 80 00 20 */	blr 
 
 .global __ct__Q34Game10MemoryCard3MgrFv
 __ct__Q34Game10MemoryCard3MgrFv:
