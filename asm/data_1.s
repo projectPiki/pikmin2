1152decdc205a4d200dd92caaa00b5561b86994b
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-10-07 21:55:36 -0400

    got it

--- asm/data_1.s
+++ asm/data_1.s
@@ -1,7 +1,7 @@
 .include "macros.inc"
 .section .data, "wa"  # 0x8049E220 - 0x804EFC20
 .balign 0x4
-
+.skip 4
 .global lbl_804ECFE8
 lbl_804ECFE8:
 	.4byte 0x00000001
@@ -3160,4 +3160,3 @@ __vt__Q23PSM18ObjCalc_SingleGame:
   .4byte 0
   .4byte 0
   .4byte 0
-.skip 4
