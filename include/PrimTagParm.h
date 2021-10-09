2752a1631dbefdbf49b9dd96916f5794ae544360
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-10-09 19:50:01 -0400

    one byte off from OK

--- include/PrimTagParm.h
+++ include/PrimTagParm.h
@@ -8,18 +8,20 @@
 #include "types.h"
 
 template <typename T> struct PrimTagParm : public TagParm {
-	 void doWrite(Stream&);	//virtual
-	 void doRead(Stream&);	//virtual
-	 void doDump();			//virtual
+	void doWrite(Stream&);	//virtual
+	void doRead(Stream&);	//virtual
+	void doDump();			//virtual
 
+	int filler;
 	T m_data; // _0C
 };
 
 struct PrimTagParm<u16> : public TagParm {
-	 void doWrite(Stream&);	//virtual
-	 void doRead(Stream&);	//virtual
-	 void doDump();			//virtual
+	void doWrite(Stream&);	//virtual
+	void doRead(Stream&);	//virtual
+	void doDump();			//virtual
 
+	int filler;
 	u16 m_data;
 };
 
@@ -27,7 +29,8 @@ struct PrimTagParm<u64> : public TagParm {
 	void doWrite(Stream&);	//virtual
 	void doRead(Stream&);	//virtual
 	void doDump();			//virtual
-
+	
+	int filler;
 	u32 _0C; // _0C
 	u8 m_data[sizeof(u64)];
 };
@@ -36,7 +39,8 @@ struct PrimTagParm<Vector3f> : public TagParm {
 	void doWrite(Stream&);	//virtual
 	void doRead(Stream&);	//virtual
 	void doDump();			//virtual
-
+	
+	int filler;
 	Vector3f m_data;
 };
 
@@ -44,7 +48,8 @@ struct PrimTagParm<int> : public TagParm {
 	void doWrite(Stream&);	//virtual
 	void doRead(Stream&);	//virtual
 	void doDump();			//virtual
-
+	
+	int filler;
 	int m_data;
 };
 
@@ -53,6 +58,7 @@ struct PrimTagParm<float> : public TagParm {
 	void doRead(Stream&);	//virtual
 	void doDump();			//virtual
 
+	int filler;
 	float m_data;
 };
 
