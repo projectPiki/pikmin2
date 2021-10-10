d64965af76e4151d98febe374475550e5d6b4172
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-10-10 01:14:44 -0400

    prgress on pelletConfig

--- include/Game/pelletConfig.h
+++ include/Game/pelletConfig.h
@@ -5,16 +5,25 @@
 #include "CNode.h"
 #include "PrimTagParm.h"
 
+struct TParms {
+	TagParameters parm1;
+};
+
 namespace Game {
 	class PelletConfig : public CNode {
 		char filler[584];
-	}; //temp filler struct
+		virtual ~PelletConfig(); //virtual destructor
+	}; //temporary filler struct for pellet config
 	class PelletConfigList : public CNode {
 		PelletConfigList();
-		//virtual ~PelletConfigList();		//virtual
-		
+		virtual ~PelletConfigList();		//virtual
+
 		int count;
 		PelletConfig* pelletConfigArray;
+		void getPelletConfig(char*);
+		void getPelletConfig(int);
+		void getPelletConfig_ByDictionaryNo(int);
+		void read(Stream&);
 	};
 } // namespace Game
 
