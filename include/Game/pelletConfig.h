dc4999b79546b8c1285343b62c7c7a886ddb5229
intns <84647527+intns@users.noreply.github.com>
2021-10-10 19:15:16 +0100

    Fix format-files and format

--- include/Game/pelletConfig.h
+++ include/Game/pelletConfig.h
@@ -1,75 +1,75 @@
 #ifndef _GAME_PELLETCONFIG_H
 #define _GAME_PELLETCONFIG_H
 
-#include "types.h"
 #include "CNode.h"
 #include "PrimTagParm.h"
+#include "types.h"
 
 namespace Game {
-	struct PelletConfig : public CNode {
-		struct TParms : public TagParameters {
-			TParms();
-			~TParms();
-			StringTagParm name;
-			StringTagParm archive;
-			StringTagParm txtArchive;
-			StringTagParm bmd;
-			StringTagParm animMgr;
-			StringTagParm colltree;
-			PrimTagParm<float> radius;
-			PrimTagParm<float> pRadius;
-			PrimTagParm<float> height;
-			PrimTagParm<float> inertiaScaling;
-			StringTagParm particleType;
-			PrimTagParm<int> numParticles;
-			PrimTagParm<float> particleSize;
-			PrimTagParm<float> friction;
-			PrimTagParm<int> min;
-			PrimTagParm<int> max;
-			PrimTagParm<int> pikiCountMax;
-			PrimTagParm<int> pikiCountMin;
-			StringTagParm dynamics;
-			PrimTagParm<int> money;
-			StringTagParm unique;
-			StringTagParm indirect;
-			PrimTagParm<int> numPMotions;
-			PrimTagParm<float> depth;
-			PrimTagParm<float> depthMax;
-			PrimTagParm<float> depthA;
-			PrimTagParm<float> depthB;
-			PrimTagParm<float> depthC;
-			PrimTagParm<float> depthD;
-			PrimTagParm<Vector3f> offset;
-			unsigned char field_0x204;
-			unsigned char field_0x205;
-			unsigned char field_0x206;
-			unsigned char field_0x207;
-			PrimTagParm<unsigned long long> message;
-			PrimTagParm<unsigned short> code;
-			unsigned char field_0x22e;
-			unsigned char field_0x22f;
-			PrimTagParm<unsigned short> dictionary;
-		}; //struct TParms
-		TParms parms;
-		short short_0x256;
-		short short_0x258;
-		unsigned char indirect;
-		unsigned char unknown[5];
-		//char filler[584];
-		PelletConfig();
-		virtual ~PelletConfig(); //virtual destructor
-	}; //temporary filler struct for pellet config
-	class PelletConfigList : public CNode {
-		PelletConfigList();
-		virtual ~PelletConfigList();		//virtual
-		
-		int count;
-		PelletConfig* pelletConfigArray;
-		void getPelletConfig(char*);
-		void getPelletConfig(int);
-		void getPelletConfig_ByDictionaryNo(int);
-		void read(Stream&);
-	};
+struct PelletConfig : public CNode {
+    struct TParms : public TagParameters {
+        TParms();
+        ~TParms();
+        StringTagParm name;
+        StringTagParm archive;
+        StringTagParm txtArchive;
+        StringTagParm bmd;
+        StringTagParm animMgr;
+        StringTagParm colltree;
+        PrimTagParm<float> radius;
+        PrimTagParm<float> pRadius;
+        PrimTagParm<float> height;
+        PrimTagParm<float> inertiaScaling;
+        StringTagParm particleType;
+        PrimTagParm<int> numParticles;
+        PrimTagParm<float> particleSize;
+        PrimTagParm<float> friction;
+        PrimTagParm<int> min;
+        PrimTagParm<int> max;
+        PrimTagParm<int> pikiCountMax;
+        PrimTagParm<int> pikiCountMin;
+        StringTagParm dynamics;
+        PrimTagParm<int> money;
+        StringTagParm unique;
+        StringTagParm indirect;
+        PrimTagParm<int> numPMotions;
+        PrimTagParm<float> depth;
+        PrimTagParm<float> depthMax;
+        PrimTagParm<float> depthA;
+        PrimTagParm<float> depthB;
+        PrimTagParm<float> depthC;
+        PrimTagParm<float> depthD;
+        PrimTagParm<Vector3f> offset;
+        unsigned char field_0x204;
+        unsigned char field_0x205;
+        unsigned char field_0x206;
+        unsigned char field_0x207;
+        PrimTagParm<unsigned long long> message;
+        PrimTagParm<unsigned short> code;
+        unsigned char field_0x22e;
+        unsigned char field_0x22f;
+        PrimTagParm<unsigned short> dictionary;
+    }; // struct TParms
+    TParms parms;
+    short short_0x256;
+    short short_0x258;
+    unsigned char indirect;
+    unsigned char unknown[5];
+    // char filler[584];
+    PelletConfig();
+    virtual ~PelletConfig(); // virtual destructor
+};                           // temporary filler struct for pellet config
+class PelletConfigList : public CNode {
+    PelletConfigList();
+    virtual ~PelletConfigList(); // virtual
+
+    int count;
+    PelletConfig* pelletConfigArray;
+    void getPelletConfig(char*);
+    void getPelletConfig(int);
+    void getPelletConfig_ByDictionaryNo(int);
+    void read(Stream&);
+};
 } // namespace Game
 
 #endif
--- include/PrimTagParm.h
+++ include/PrimTagParm.h
@@ -8,61 +8,61 @@
 #include "types.h"
 
 template <typename T> struct PrimTagParm : public TagParm {
-	virtual void doWrite(Stream&);	//virtual
-	virtual void doRead(Stream&);	//virtual
-	virtual void doDump();			//virtual
+    virtual void doWrite(Stream&); // virtual
+    virtual void doRead(Stream&);  // virtual
+    virtual void doDump();         // virtual
 
-	T m_data; // _0C
+    T m_data; // _0C
 };
 
 struct PrimTagParm<u16> : public TagParm {
-	virtual void doWrite(Stream&);	//virtual
-	virtual void doRead(Stream&);	//virtual
-	virtual void doDump();			//virtual
+    virtual void doWrite(Stream&); // virtual
+    virtual void doRead(Stream&);  // virtual
+    virtual void doDump();         // virtual
 
-	u16 m_data;
+    u16 m_data;
 };
 
 struct PrimTagParm<u64> : public TagParm {
-	virtual void doWrite(Stream&);	//virtual
-	virtual void doRead(Stream&);	//virtual
-	virtual void doDump();			//virtual
+    virtual void doWrite(Stream&); // virtual
+    virtual void doRead(Stream&);  // virtual
+    virtual void doDump();         // virtual
 
-	u32 _0C; // _0C
-	//u8 m_data[sizeof(u64)];
-	u32 m_data_1;
-	u32 m_data_2;
+    u32 _0C; // _0C
+    // u8 m_data[sizeof(u64)];
+    u32 m_data_1;
+    u32 m_data_2;
 };
 
 struct PrimTagParm<Vector3f> : public TagParm {
-	virtual void doWrite(Stream&);	//virtual
-	virtual void doRead(Stream&);	//virtual
-	virtual void doDump();			//virtual
+    virtual void doWrite(Stream&); // virtual
+    virtual void doRead(Stream&);  // virtual
+    virtual void doDump();         // virtual
 
-	Vector3f m_data;
+    Vector3f m_data;
 };
 
 struct PrimTagParm<int> : public TagParm {
-	virtual void doWrite(Stream&);	//virtual
-	virtual void doRead(Stream&);	//virtual
-	virtual void doDump();			//virtual
+    virtual void doWrite(Stream&); // virtual
+    virtual void doRead(Stream&);  // virtual
+    virtual void doDump();         // virtual
 
-	int m_data;
+    int m_data;
 };
 
 struct PrimTagParm<float> : public TagParm {
-	virtual void doWrite(Stream&);	//virtual
-	virtual void doRead(Stream&);	//virtual
-	virtual void doDump();			//virtual
+    virtual void doWrite(Stream&); // virtual
+    virtual void doRead(Stream&);  // virtual
+    virtual void doDump();         // virtual
 
-	float m_data;
+    float m_data;
 };
 
 struct TagParameters : public CNode {
-	TagParameters(char*);
-	virtual ~TagParameters();		//virtual
+    TagParameters(char*);
+    virtual ~TagParameters(); // virtual
 
-	u32 _18; // _18
+    u32 _18; // _18
 };
 
 #endif
