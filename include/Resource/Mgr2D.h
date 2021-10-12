81de15aaa7deb44b4935e9e0bf0495f875b6c95d
intns <84647527+intns@users.noreply.github.com>
2021-10-12 13:34:37 +0100

    Create Mgr2D.h

---
+++ include/Resource/Mgr2D.h
@@ -0,0 +1,14 @@
+#ifndef _RESOURCE_MGR2D_H
+#define _RESOURCE_MGR2D_H
+
+#include "JSystem/JKRHeap.h"
+
+namespace Resource {
+struct Mgr2D {
+	// Mgr2D(JKRHeap*); <-- Unused
+
+	void init(JKRHeap*);
+};
+} // namespace Resource
+
+#endif
