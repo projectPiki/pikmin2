1a4b00b62c8dec34c36fc3cb3b7929b489774b13
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-10-16 20:52:07 -0400

    nonmatching gcn memalloc

--- src/Dolphin/GCN_mem_alloc.c
+++ src/Dolphin/GCN_mem_alloc.c
@@ -1,63 +1,41 @@
+#include "types.h"
+#include "Dolphin/os.h"
 
+// nonmatching
 
 /*
  * --INFO--
  * Address:	800C23A8
  * Size:	0000B8
  */
-void __sys_free(void)
-{
-/*
-.loc_0x0:
-  stwu      r1, -0x20(r1)
-  mflr      r0
-  stw       r0, 0x24(r1)
-  stw       r31, 0x1C(r1)
-  stw       r30, 0x18(r1)
-  stw       r29, 0x14(r1)
-  mr        r29, r3
-  lwz       r0, -0x7CA8(r13)
-  cmpwi     r0, -0x1
-  bne-      .loc_0x90
-  lis       r3, 0x8048
-  subi      r3, r3, 0x60C8
-  crclr     6, 0x6
-  bl        0x2B310
-  lis       r3, 0x8048
-  subi      r3, r3, 0x6090
-  crclr     6, 0x6
-  bl        0x2B300
-  bl        0x2A03C
-  mr        r31, r3
-  bl        0x2A02C
-  mr        r30, r3
-  mr        r3, r31
-  li        r5, 0x1
-  mr        r4, r30
-  bl        0x29F3C
-  mr        r31, r3
-  bl        0x2A028
-  addi      r0, r31, 0x1F
-  rlwinm    r30,r30,0,0,26
-  rlwinm    r3,r0,0,0,26
-  mr        r4, r30
-  bl        0x29F90
-  bl        0x29F0C
-  mr        r3, r30
-  bl        0x2A008
+extern int __OSCurrHeap;
+extern int OSGetArenaLo();
+extern int OSGetArenaHi();
+extern int OSInitAlloc(int, uint, int);
+extern void OSSetArenaLo(uint);
+extern int OSCreateHeap(int, uint);
+extern uint OSSetCurrentHeap();
+extern void OSFreeToHeap(int, int);
+void __sys_free(uint param_1)
 
-.loc_0x90:
-  lwz       r3, -0x7CA8(r13)
-  mr        r4, r29
-  bl        0x29E7C
-  lwz       r0, 0x24(r1)
-  lwz       r31, 0x1C(r1)
-  lwz       r30, 0x18(r1)
-  lwz       r29, 0x14(r1)
-  mtlr      r0
-  addi      r1, r1, 0x20
-  blr
-*/
+{
+	uint uVar1;
+	uint uVar2;
+	int iVar3;
+
+	if (__OSCurrHeap == -1) {
+		OSReport("GCN_Mem_Alloc.c : InitDefaultHeap. No Heap Available\n");
+		OSReport("Metrowerks CW runtime library initializing default heap\n");
+		uVar1 = OSGetArenaLo();
+		uVar2 = OSGetArenaHi();
+		iVar3 = OSInitAlloc(uVar1, uVar2, 1);
+		OSSetArenaLo(uVar2);
+		OSCreateHeap(iVar3 + 0x1fU & 0xffffffe0, uVar2 & 0xffffffe0);
+		OSSetCurrentHeap();
+		OSSetArenaLo(uVar2 & 0xffffffe0);
+	}
+	OSFreeToHeap(__OSCurrHeap, param_1);
+	return;
 }
 
 /*
