c91a5744d06725b1f5af92973eebe39b28c913ed
intns <84647527+intns@users.noreply.github.com>
2021-10-18 14:36:21 +0100

    Start TParse_TParagraph (wip)

--- src/JSystem/fvb/fvb-data-parse.cpp
+++ src/JSystem/fvb/fvb-data-parse.cpp
@@ -1,44 +1,49 @@
+#include "types.h"
 
+namespace JGadget {
+namespace binary {
+	struct TEBit {
+	};
 
-/*
- * --INFO--
- * Address:	8000CA3C
- * Size:	000068
- */
-void JStudio::fvb::data::TParse_TParagraph::getData(
-    const(JStudio::fvb::data::TParse_TParagraph::TData*))
-{
-	/*
-	.loc_0x0:
-	  stwu      r1, -0x10(r1)
-	  mflr      r0
-	  lwz       r3, 0x0(r3)
-	  li        r6, 0
-	  stw       r0, 0x14(r1)
-	  stw       r31, 0xC(r1)
-	  mr        r31, r4
-	  addi      r5, r31, 0x4
-	  bl        0x1A6CC
-	  lwz       r4, 0x0(r31)
-	  cmplwi    r4, 0
-	  bne-      .loc_0x40
-	  li        r0, 0
-	  stw       r0, 0x8(r31)
-	  stw       r3, 0xC(r31)
-	  b         .loc_0x54
+	extern u16* parseVariableUInt_16_32_following(void const*, unsigned long*,
+	                                              unsigned long*, TEBit*);
+} // namespace binary
+} // namespace JGadget
 
-	.loc_0x40:
-	  addi      r0, r4, 0x3
-	  stw       r3, 0x8(r31)
-	  rlwinm    r0,r0,0,0,29
-	  add       r0, r3, r0
-	  stw       r0, 0xC(r31)
+namespace JStudio {
+namespace fvb {
+	namespace data {
 
-	.loc_0x54:
-	  lwz       r0, 0x14(r1)
-	  lwz       r31, 0xC(r1)
-	  mtlr      r0
-	  addi      r1, r1, 0x10
-	  blr
-	*/
-}
+		struct TParse_TParagraph {
+			struct TData {
+				u32* _00;
+				u32* _04;
+				u16* _08;
+				u16* _0C;
+			};
+
+			void getData(TData*) const;
+		};
+
+		/*
+		 * --INFO--
+		 * Address:	8000CA3C
+		 * Size:	000068
+		 */
+		void JStudio::fvb::data::TParse_TParagraph::getData(
+		    TParse_TParagraph::TData* data) const
+		{
+			u16* v3 = JGadget::binary::parseVariableUInt_16_32_following(
+			    (void*)this, data->_00, data->_04, nullptr);
+			if (data->_00) {
+				data->_08 = v3;
+				data->_0C = v3 + ((data->_00 + 3) & ~4);
+			} else {
+				data->_08 = 0;
+				data->_0C = v3;
+			}
+		}
+
+	} // namespace data
+} // namespace fvb
+} // namespace JStudio
