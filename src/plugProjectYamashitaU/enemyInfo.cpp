3ab47b0830504087a7269020fab995902e71fb9a
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-10-29 17:22:20 -0400

    gEnemyInfo structified

--- src/plugProjectYamashitaU/enemyInfo.cpp
+++ src/plugProjectYamashitaU/enemyInfo.cpp
@@ -1,29 +1,5 @@
 #include "Game/enemyInfo.h"
 namespace Game {
-// clang-format off
-	struct EnemyInfo gEnemyInfo[100] = {
-		//  name                    ID    pID  members flags    model       anim        animgr      texture     param        collision  stone     childID  childNum droptype
-			{"Pelplant",          0x0,   -1,   1,      0x0007,  "Pelplant", "Pelplant", "Pelplant", "Pelplant", "Pelplant", "Pelplant", "Pelplant", -1,       0,    4},
-			{"Kochappy",          0x1,   -1,   1,      0x0117,  "",         "",         "",         "",         "",         "",         "",         -1,       0,    1},
-			{"BlueKochappy",     0x2c,   -1,   1,      0x0117,  "Kochappy", "Kochappy", "Kochappy", "",         "",         "Kochappy", "Kochappy", -1,       0,    1},
-			{"YellowKochappy",   0x2d,   -1,   1,      0x0117,  "Kochappy", "Kochappy", "Kochappy", "",         "",         "Kochappy", "Kochappy", -1,       0,    1},
-			{"KumaKochappy",     0x4c,   -1,   1,      0x0117,  "",         "Kochappy", "Kochappy", "",         "",         "Kochappy", "",         -1,       0,    1},
-			{"Chappy",            0x2,   -1,   1,      0x0097,  "",         "",         "",         "",         "",         "",         "",         -1,       0,    2},
-			{"BlueChappy",       0x2a,   -1,   1,      0x0097,  "Chappy",   "Chappy",   "Chappy",   "",         "",         "Chappy",   "Chappy",   -1,       0,    2},
-			{"YellowChappy",     0x2b,   -1,   1,      0x0097,  "Chappy",   "Chappy",   "Chappy",   "",         "",         "Chappy",   "Chappy",   -1,       0,    2},
-			{"Pom",              0x52,   -1,   1,      0x0001,  "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      -1,       0,    4},
-			{"BluePom",           0x3, 0x52,   1,      0x0006,  "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      -1,       0,    4},
-			{"RedPom",            0x4, 0x52,   1,      0x0006,  "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      -1,       0,    4},
-			{"YellowPom",         0x5, 0x52,   1,      0x0006,  "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      -1,       0,    4},
-			{"BlackPom",          0x6, 0x52,   1,      0x0006,  "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      -1,       0,    4},
-			{"WhitePom",          0x7, 0x52,   1,      0x0006,  "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      -1,       0,    4},
-			{"RandPom",           0x8, 0x52,   1,      0x0006,  "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      "Pom",      -1,       0,    4},
-			{"Kogane",            0x9,   -1,   1,      0x0007,  "",         "",         "",         "",         "",         "",         "",         -1,       0,    4},
-			{"Wealthy",           0xa,   -1,   1,      0x0007,  "Kogane",   "Kogane",   "Kogane",   "",         "",         "Kogane",   "Kogane",   -1,       0,    4},
-			{"Fart",              0xb,   -1,   1,      0x0007,  "Kogane",   "Kogane",   "Kogane",   "",         "",         "Kogane",   "Kogane",   -1,       0,    4},
-	};
-// clang-format on
-extern int gEnemyInfoNum = 100;
 namespace EnemyInfoFunc {
 	EnemyInfo* getEnemyInfo(int enemyID, int enemyFlags)
 	{
