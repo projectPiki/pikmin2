c028cd07fc8faefe6774b9b321722a65e1ab78cc
SodiumChlorideLogic <19625889+SodiumChlorideLogic@users.noreply.github.com>
2021-10-31 15:30:52 -0400

    getEnemyName and WIP of getEnemyResName.
    Co-Authored-By: EpochFlame <82550703+EpochFlame@users.noreply.github.com>

--- src/plugProjectYamashitaU/enemyInfo.cpp
+++ src/plugProjectYamashitaU/enemyInfo.cpp
@@ -20,5 +20,42 @@ namespace EnemyInfoFunc {
 		}
 		return result;
 	}
+
+	/**
+	 * Common loop used by at least getEnemyName.
+	 */
+	inline EnemyInfo* getInfo(int enemyID, int enemyFlags) {
+		EnemyInfo* result = nullptr;
+		for (int enemy_num = gEnemyInfoNum, slot = 0; enemy_num > 0; --enemy_num) {
+			if ((enemyID == gEnemyInfo[slot].ID)
+				&& ((enemyFlags & (uint)gEnemyInfo[slot].flags) != 0)) {
+				result = &gEnemyInfo[slot];
+				break;
+			}
+			++slot;
+		}
+		return result;
+	}
+
+	char* getEnemyName(int enemyID, int enemyFlags)
+	{
+		EnemyInfo* result = getInfo(enemyID, enemyFlags);
+		return (result) ? result->name : nullptr;
+	}
+
+
+	// TODO: Not Matching.
+	char* getEnemyResName(int enemyID, int enemyFlags) {
+		if (enemyID < 0x3f) {
+			if (0x3b < enemyID) {
+				return "Otakara";
+			}
+		}
+		if ((enemyID == 0x27) || (enemyID == 0x40)) {
+			enemyID = PanHouse;
+		}
+		EnemyInfo* result = getInfo(enemyID, enemyFlags);
+		return (result->anim_name == "") ? result->name : result->anim_name;
+	}
 } // namespace EnemyInfoFunc
 } // namespace Game
