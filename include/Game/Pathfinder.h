b2254cb12ae9aacfbfdac851068a0111eca0bef5
intns <84647527+intns@users.noreply.github.com>
2021-11-01 03:19:39 +0000

    Cleanup ObjectTypes

--- include/Game/Pathfinder.h
+++ include/Game/Pathfinder.h
@@ -5,19 +5,20 @@
 
 namespace Game {
 struct Pathfinder {
-	uint idk;                         // _00
-	int clientCount;                  // _04
-	int aStarContextCount;            // _08
-	AStarContext* aStarContexts;      // _0C
-	AStarPathfinder* aStarPathfinder; // _10
+	u32 _00;                                 // _00
+	s32 m_clientCount;                       // _04
+	s32 aStarContextCount;                   // _08
+	struct AStarContext* aStarContexts;      // _0C
+	struct AStarPathfinder* aStarPathfinder; // _10
 
 	Pathfinder();
-	void create(int, RouteMgr*);
+	void create(int, struct RouteMgr*);
 	void update();
-	int start(PathfindRequest*);
-	int makepath(ulong, PathNode**);
+	int start(struct PathfindRequest*);
+	int makepath(ulong, struct PathNode**);
 	void release(ulong);
 	uchar check(ulong);
 };
 } // namespace Game
+
 #endif
