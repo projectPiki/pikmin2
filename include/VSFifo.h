8ff5f3e889aebc6dc2fc6aff71377113bab61b6c
intns <84647527+intns@users.noreply.github.com>
2021-11-02 03:51:41 +0000

    Update VSFifo.h

--- include/VSFifo.h
+++ include/VSFifo.h
@@ -1,12 +1,19 @@
 #ifndef _VSFIFO_H
 #define _VSFIFO_H
 
+#include "types.h"
+
 struct VSFifo {
 	VSFifo(unsigned long);
 	~VSFifo();
 
 	void becomeCurrent();
 	void isGPActive();
+
+	u32 _00;       // _00
+	int* m_gpFifo; // _04
+	int* _08;      // _08
+	int _0C;       // _0C
 };
 
 #endif
