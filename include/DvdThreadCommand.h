b6ef58cf28849abacfcf8ab9792a1e0cd83620ff
intns <84647527+intns@users.noreply.github.com>
2021-11-02 03:35:18 +0000

    Fill System struct

---
+++ include/DvdThreadCommand.h
@@ -0,0 +1,27 @@
+#ifndef _DVDTHREADCOMMAND_H
+#define _DVDTHREADCOMMAND_H
+
+#include "types.h"
+#include "JSystem/JSU/JSUPtrLink.h"
+
+struct JKRArchive;
+struct JKRHeap;
+
+struct DvdThreadCommand {
+	u32 _00;                      // _00
+	u32 m_loadType;               // _04
+	char* m_arcPath;              // _08
+	JKRArchive* m_mountedArchive; // _0C
+	s32 _10;                      // _10
+	u32 m_modeFlags;              // _14
+	s32 m_mode;                   // _18
+	char m_os;                    // _1C
+	u8 _1D[0x16];                 // _1D
+	JKRHeap* m_heap;              // _34
+	u8 _38[0x8];                  // _38
+	s32* _40;                     // _40
+	u8 _44[0x18];                 // _44
+	JSUPtrLink m_link;            // _5C
+};
+
+#endif
--- include/System.h
+++ include/System.h
@@ -2,6 +2,7 @@
 #define _SYSTEM_H
 
 #include "types.h"
+#include "DvdThreadCommand.h"
 
 void Pikmin2DefaultMemoryErrorRoutine(void*, u32, s32);
 void kando_panic_f(bool, const char*, s32, const char*, ...);
@@ -76,11 +77,34 @@ struct System {
 	bool dvdLoadSyncNoBlock(struct DvdThreadCommand*);
 	int dvdLoadSyncAllNoBlock();
 
-	u8 _00[0x3C];            // _00
-	u32* _3C;                // _3C
-	u8 _40[0x14];            // _40
-	float m_secondsPerFrame; // _54
-	u8 _58[0x88];            // _58
+	f32 _00;                           // _00
+	f32 _04;                           // _04
+	f32 _08;                           // _08
+	u8 _0C[0xC];                       // _0C
+	struct JKRHeap* _18;               // _18
+	u32 m_cpuRetraceCount;             // _1C
+	u32 m_cpuLockCount;                // _20
+	struct Graphics* m_gfx;            // _24
+	struct SysTimers* m_timers;        // _28
+	u32 _2C;                           // _2C
+	u32 _30;                           // _30
+	u32 _34;                           // _34
+	struct JKRHeap* m_sysHeap;         // _38
+	struct GameFlow* m_gameFlow;       // _3C
+	struct DvdThread* m_dvdThread;     // _40
+	struct ResetManager* m_resetMgr;   // _44
+	struct DvdStatus* m_dvdStatus;     // _48
+	struct JFWDisplay* m_display;      // _4C
+	struct HeapStatus* m_heapStatus;   // _50
+	float m_secondsPerFrame;           // _54
+	struct JKRTask* m_task;            // _58
+	struct MemoryCardMgr* m_cardMgr;   // _5C
+	struct PlayCommonData* m_playData; // _60
+	float m_fpsFactor;                 // _64
+	DvdThreadCommand m_threadCommand;  // _68
+	int m_region;                      // _D4
+	u32 m_flags;                       // _D8
+	struct JUTRomFont* m_romFont;      // _DC
 };
 
 extern System* const sys;
