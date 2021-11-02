609654c498595d10bf67fe6cbe159db177830803
intns <84647527+intns@users.noreply.github.com>
2021-11-02 04:15:56 +0000

    Update generalEnemyMgr.h

--- include/Game/generalEnemyMgr.h
+++ include/Game/generalEnemyMgr.h
@@ -17,27 +17,24 @@ struct EnemyMgrNode : public CNode, GenericObjectMgr {
 	virtual void startMovie();
 	virtual void endMovie();
 
-	// CNode = _00-_18
 	// GenericObjectMgr+EnemyMgrNode VT = _18
-	EEnemyTypeID m_enemyID; // _1C
-	// TODO: replace with proper type
-	// EnemyMgrBase* m_mgr;    // _20
-	void* m_mgr;    // _20
+	EEnemyTypeID m_enemyID;     // _1C
+	struct EnemyMgrBase* m_mgr; // _20
 };
 
 struct GeneralEnemyMgr : public GenericObjectMgr {
 	EEnemyTypeID getEnemyID(char*, int);
 	char* getEnemyName(EEnemyTypeID, int);
 
-	CNode _04;                  // _04
-	u8 _1C;                     // _1C
-	u8 _1D[3];                  // _1D
-	EnemyMgrNode _20;           // _20
-	u8 _44[4];                  // _44
-	int _48;                    // _48
-	JKRHeap* m_heap;            // _4C
-	ulong m_heapSize;           // _50
-	uint _54;                   // _54
+	CNode _04;                    // _04
+	u8 _1C;                       // _1C
+	u8 _1D[3];                    // _1D
+	EnemyMgrNode _20;             // _20
+	u8 _44[4];                    // _44
+	EEnemyTypeID* m_enemyNumList; // _48
+	JKRHeap* m_heap;              // _4C
+	ulong m_heapSize;             // _50
+	uint m_flags;                 // _54
 	// TODO: replace with proper type
 	// EnemyStone::Mgr m_stoneMgr; // _58
 	u8 m_stoneMgr[0x38]; // _58
@@ -46,6 +43,5 @@ struct GeneralEnemyMgr : public GenericObjectMgr {
 static GeneralEnemyMgr* generalEnemyMgr;
 
 }; // namespace Game
-
 
 #endif
