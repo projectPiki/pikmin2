7f6b57578826462302c05ccf35fa8d46e1276b4b
intns <84647527+intns@users.noreply.github.com>
2021-11-07 13:21:57 +0000

    Update TParse.h

--- include/JSystem/fvb/TParse.h
+++ include/JSystem/fvb/TParse.h
@@ -5,13 +5,6 @@
 
 namespace JStudio {
 namespace fvb {
-	struct TControl;
-
-	struct TParse {
-		TParse(TControl*);
-		virtual ~TParse();
-	};
-
 	namespace data {
 		struct TParse_TParagraph {
 			struct TData {
@@ -23,7 +16,90 @@ namespace fvb {
 
 			void getData(TData*) const;
 		};
+
+		// TODO: Define TParse_TBlock
+		struct TParse_TBlock {
+		};
 	} // namespace data
+
+	struct TControl {
+		virtual ~TControl(); // _00
+
+		// _00 VTBL
+	};
+
+	struct TObject {
+		virtual ~TObject() = 0; // _00
+		virtual void prepare_data_(const data::TParse_TParagraph::TData&,
+		                           TControl*); // _04
+
+		// _00 VTBL
+	};
+
+	struct TObject_composite : public TObject {
+		virtual ~TObject_composite(); // _00
+		virtual void prepare_data_(const data::TParse_TParagraph::TData&,
+		                           TControl*); // _04
+
+		// _00 VTBL
+	};
+
+	struct TObject_transition : public TObject {
+		virtual ~TObject_transition(); // _00
+		virtual void prepare_data_(const data::TParse_TParagraph::TData&,
+		                           TControl*); // _04
+
+		// _00 VTBL
+	};
+
+	struct TObject_list_parameter : public TObject {
+		virtual ~TObject_list_parameter(); // _00
+		virtual void prepare_data_(const data::TParse_TParagraph::TData&,
+		                           TControl*); // _04
+
+		// _00 VTBL
+	};
+
+	struct TObject_list : public TObject {
+		virtual ~TObject_list(); // _00
+		virtual void prepare_data_(const data::TParse_TParagraph::TData&,
+		                           TControl*); // _04
+
+		// _00 VTBL
+	};
+
+	struct TObject_constant : public TObject {
+		virtual ~TObject_constant(); // _00
+		virtual void prepare_data_(const data::TParse_TParagraph::TData&,
+		                           TControl*); // _04
+
+		// _00 VTBL
+	};
+
+	struct TObject_hermite : public TObject {
+		virtual ~TObject_hermite(); // _00
+		virtual void prepare_data_(const data::TParse_TParagraph::TData&,
+		                           TControl*); // _04
+
+		// _00 VTBL
+	};
+
+	struct TFactory {
+		virtual ~TFactory();                             // _00
+		virtual void create(const data::TParse_TBlock&); // _04
+		virtual void destroy(TObject*);                  // _08
+
+		// _00 VTBL
+	};
+
+	struct TParse {
+		TParse(TControl*);
+		virtual ~TParse(); // _00
+		virtual void parseHeader_next(const void**, unsigned long*,
+		                              unsigned long); // _04
+		virtual void parseBlock_next(const void**, unsigned long*,
+		                             unsigned long); // _08
+	};
 } // namespace fvb
 } // namespace JStudio
 
