d3dc5fb389f6d0d4801b933d46a5c67cc20897cf
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2021-11-30 20:23:19 -0500

    cleanup and update rand.c

--- src/Dolphin/rand.c
+++ src/Dolphin/rand.c
@@ -10,11 +10,6 @@ u32 next = 1;
 void srand(u32 seed)
 {
 	next = seed;
-	/*
-  .loc_0x0:
-	stw       r3, -0x7DD8(r13)
-	blr
-  */
 }
 
 /*
@@ -24,17 +19,6 @@ void srand(u32 seed)
  */
 s16 rand(void)
 {
-	next = next * 0x41c64e6d + 0x3039;
+	next = next * 1103515245 + 12345;
 	return (s16)((u16)((u32)next >> 0x10) & 0x7fff);
-	/*
-  .loc_0x0:
-	lis       r3, 0x41C6
-	lwz       r4, -0x7DD8(r13)
-	addi      r0, r3, 0x4E6D
-	mullw     r3, r4, r0
-	addi      r0, r3, 0x3039
-	stw       r0, -0x7DD8(r13)
-	rlwinm    r3,r0,16,17,31
-	blr
-  */
 }
