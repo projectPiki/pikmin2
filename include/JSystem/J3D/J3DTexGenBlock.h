358646e3eb878e4dca8e0f5c4983bb92f1efd33f
SodiumChlorideLogic <19625889+SodiumChlorideLogic@users.noreply.github.com>
2021-12-15 15:18:00 -0500

    J3DTexGenBlock.h

---
+++ include/JSystem/J3D/J3DTexGenBlock.h
@@ -0,0 +1,116 @@
+#ifndef _JSYSTEM_J3D_J3DTEXGENBLOCK_H
+#define _JSYSTEM_J3D_J3DTEXGENBLOCK_H
+
+#include "JSystem/J3D/J3DTypes.h"
+#include "types.h"
+
+struct J3DNBTScale;
+struct J3DTexMtx;
+
+struct J3DTexGenBlock {
+	virtual void reset(J3DTexGenBlock*);                               // _00
+	virtual void calc(const float (*)[4])                         = 0; // _04
+	virtual void calcWithoutViewMtx(const float (*)[4])           = 0; // _08
+	virtual void calcPostTexMtx(const float (*)[4])               = 0; // _0C
+	virtual void calcPostTexMtxWithoutViewMtx(const float (*)[4]) = 0; // _10
+	virtual void load()                                           = 0; // _14
+	virtual void patch()                                          = 0; // _18
+	virtual void diff(ulong)                                      = 0; // _1C
+	virtual void diffTexMtx()                                     = 0; // _20
+	virtual void diffTexGen()                                     = 0; // _24
+	virtual uint countDLSize();                                        // _28
+	virtual JBlockType getType() = 0;                                  // _2C
+	virtual void setTexGenNum(const ulong*);                           // _30
+	virtual void setTexGenNum(ulong);                                  // _34
+	virtual ulong getTexGenNum() const;                                // _38
+	virtual void setTexCoord(ulong, const J3DTexCoord*);               // _3C
+	virtual J3DTexCoord* getTexCoord(ulong);                           // _40
+	virtual void setTexMtx(ulong, J3DTexMtx*);                         // _44
+	virtual J3DTexMtx* getTexMtx(ulong);                               // _48
+	virtual void setNBTScale(const J3DNBTScale*);                      // _4C
+	virtual void setNBTScale(J3DNBTScale);                             // _50
+	virtual J3DNBTScale* getNBTScale();                                // _54
+	virtual ulong getTexMtxOffset() const;                             // _58
+	virtual void setTexMtxOffset(ulong);                               // _5C
+	virtual ~J3DTexGenBlock();                                         // _60
+
+	// VTBL _00
+};
+
+/**
+ * @size{0x4}
+ */
+struct J3DTexGenBlockNull : public J3DTexGenBlock {
+	virtual void calc(const float (*)[4]);                         // _04
+	virtual void calcWithoutViewMtx(const float (*)[4]);           // _08
+	virtual void calcPostTexMtx(const float (*)[4]);               // _0C
+	virtual void calcPostTexMtxWithoutViewMtx(const float (*)[4]); // _10
+	virtual void load();                                           // _14
+	virtual void patch();                                          // _18
+	virtual void diff(ulong);                                      // _1C
+	virtual void diffTexMtx();                                     // _20
+	virtual void diffTexGen();                                     // _24
+	virtual JBlockType getType();                                  // _2C
+	virtual ~J3DTexGenBlockNull();                                 // _60
+};
+
+struct J3DTexGenBlockPatched : public J3DTexGenBlock {
+	virtual void reset(J3DTexGenBlock*);                           // _00
+	virtual void calc(const float (*)[4]);                         // _04
+	virtual void calcWithoutViewMtx(const float (*)[4]);           // _08
+	virtual void calcPostTexMtx(const float (*)[4]);               // _0C
+	virtual void calcPostTexMtxWithoutViewMtx(const float (*)[4]); // _10
+	virtual void load();                                           // _14
+	virtual void patch();                                          // _18
+	virtual void diff(ulong);                                      // _1C
+	virtual void diffTexMtx();                                     // _20
+	virtual void diffTexGen();                                     // _24
+	virtual uint countDLSize();                                    // _28
+	virtual JBlockType getType();                                  // _2C
+	virtual void setTexGenNum(const ulong*);                       // _30
+	virtual void setTexGenNum(ulong);                              // _34
+	virtual ulong getTexGenNum() const;                            // _38
+	virtual void setTexCoord(ulong, const J3DTexCoord*);           // _3C
+	virtual J3DTexCoord* getTexCoord(ulong);                       // _40
+	virtual void setTexMtx(ulong, J3DTexMtx*);                     // _44
+	virtual J3DTexMtx* getTexMtx(ulong);                           // _48
+	virtual ulong getTexMtxOffset() const;                         // _58
+	virtual void setTexMtxOffset(ulong);                           // _5C
+	virtual ~J3DTexGenBlockPatched();                              // _60
+
+	// VTBL _00
+	ulong m_texgenCnt;           // _04
+	J3DTexCoord m_texCoords[8];  // _08
+	J3DTexMtx* m_texMatrices[8]; // _38
+	u32* m_dlistOffs;            // _58
+};
+
+struct J3DTexGenBlockBasic : public J3DTexGenBlockPatched {
+	virtual void reset(J3DTexGenBlock*);          // _00
+	virtual void load();                          // _14
+	virtual void patch();                         // _18
+	virtual uint countDLSize();                   // _28
+	virtual JBlockType getType();                 // _2C
+	virtual void setNBTScale(const J3DNBTScale*); // _4C
+	virtual void setNBTScale(J3DNBTScale);        // _50
+	virtual J3DNBTScale* getNBTScale();           // _54
+	virtual ~J3DTexGenBlockBasic();               // _60
+
+	J3DNBTScale m_nbtScale; // _5C
+};
+
+struct J3DTexGenBlock4 : public J3DTexGenBlockPatched {
+	virtual void reset(J3DTexGenBlock*);          // _00
+	virtual void load();                          // _14
+	virtual void patch();                         // _18
+	virtual uint countDLSize();                   // _28
+	virtual JBlockType getType();                 // _2C
+	virtual void setNBTScale(const J3DNBTScale*); // _4C
+	virtual void setNBTScale(J3DNBTScale);        // _50
+	virtual J3DNBTScale* getNBTScale();           // _54
+	virtual ~J3DTexGenBlock4();                   // _60
+
+	J3DNBTScale m_nbtScale; // _5C
+};
+
+#endif
--- include/JSystem/J3D/J3DTypes.h
+++ include/JSystem/J3D/J3DTypes.h
@@ -9,4 +9,49 @@
  */
 typedef uint J3DModelLoaderFlags;
 
+// TODO: Convert to '' notation when not lazy.
+enum JBlockType {
+	COLOR_AMBIENT_ON = 0x434c4142,
+	COLOR_NULL       = 0x434c4e4c,
+	COLOR_LIGHT_OFF  = 0x434c4f46,
+	COLOR_LIGHT_ON   = 0x434c4f4e,
+	IND_FULL         = 0x49424c46,
+	IND_NULL         = 0x49424c4e,
+	PE_TEX_EDGE      = 0x50454544,
+	PE_FOG_OFF       = 0x50454647,
+	PE_FULL          = 0x5045464c,
+	PE_NULL          = 0x50454e4c,
+	PE_OPA           = 0x50454f50,
+	PE_XLU           = 0x5045584c,
+	TEX_GEN_4        = 0x54474234,
+	TEX_GEN_BASIC    = 0x54474243,
+	TEX_GEN_NULL     = 0x54474e4c,
+	TEX_GEN_PATCHED  = 0x54475054,
+	TEV_16           = 0x54563136,
+	TEV_1            = 0x54564231,
+	TEV_2            = 0x54564232,
+	TEV_4            = 0x54564234,
+	TEV_8            = 0x54564238,
+	TEV_NULL         = 0x54564e4c,
+	TEV_PATCHED      = 0x54565054
+};
+
+// TODO: Determine if this needs packing pragmas to make it exactly 6 bytes
+/**
+ * @size{0x6}
+ */
+struct J3DTexCoord {
+	u8 _00;     // _00
+	u8 _01;     // _01
+	u8 _02;     // _02
+	ushort _04; // _04
+};
+
+struct J3DNBTScale {
+	u8 _00;    // _00
+	float _04; // _04
+	float _08; // _08
+	float _0C; // _0C
+};
+
 #endif
