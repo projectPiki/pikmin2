0fb641548aabd6765610ed544e7b44ffcd405186
SodiumChlorideLogic <19625889+SodiumChlorideLogic@users.noreply.github.com>
2021-12-15 14:36:15 -0500

    J3DMaterial.h

---
+++ include/JSystem/J3D/J3DMaterial.h
@@ -0,0 +1,105 @@
+#ifndef _JSYSTEM_J3D_J3DMATERIAL_H
+#define _JSYSTEM_J3D_J3DMATERIAL_H
+
+#include "JSystem/J3D/J3DTypes.h"
+#include "types.h"
+
+struct J3DColorBlock;
+struct J3DDisplayListObj;
+struct J3DIndBlock;
+struct J3DJoint;
+struct J3DMaterialAnm;
+struct J3DPEBlock;
+struct J3DShape;
+struct J3DTevBlock;
+struct J3DTexGenBlock;
+
+/**
+ * @size{0x4C}
+ */
+struct J3DMaterial {
+	J3DMaterial();
+	~J3DMaterial();
+
+	virtual void calc(const float (*)[4]);           // _00
+	virtual void calcDiffTexMtx(const float (*)[4]); // _04
+	virtual void makeDisplayList();                  // _08
+	virtual void makeSharedDisplayList();            // _0C
+	virtual void load();                             // _10
+	virtual void loadSharedDL();                     // _14
+	virtual void patch();                            // _18
+	virtual void diff(ulong);                        // _1C
+	virtual void reset();                            // _20
+	virtual void change();                           // _24
+
+	void calcCurrentMtx();
+	uint calcSizePEBlock(J3DModelLoaderFlags, ulong);
+	uint calcSizeTevBlock(int);
+	uint calcSizeTexGenBlock(J3DModelLoaderFlags);
+	uint countDLSize();
+	J3DColorBlock* createColorBlock(ulong);
+	J3DIndBlock* createIndBlock(int);
+	J3DPEBlock* createPEBlock(ulong, ulong);
+	J3DTevBlock* createTevBlock(int);
+	J3DTexGenBlock* createTexGenBlock(ulong);
+	void initialize();
+	int newSharedDisplayList(ulong);
+	int newSingleSharedDisplayList(ulong);
+	void setCurrentMtx();
+
+	// VTBL _00
+	u32 _04;                       // _04
+	J3DShape* m_shape;             // _08
+	J3DJoint* m_joint;             // _0C
+	u32 _10;                       // _10
+	short _14;                     // _14
+	u32 _18;                       // _18
+	u8 _1C[4];                     // _1C
+	J3DDisplayListObj* _20;        // _20
+	J3DColorBlock* m_colorBlock;   // _24
+	J3DTexGenBlock* m_texGenBlock; // _28
+	J3DTevBlock* m_tevBlock;       // _2C
+	J3DIndBlock* m_indBlock;       // _30
+	J3DPEBlock* m_peBlock;         // _34
+	J3DMaterial* _38;              // _38
+	J3DMaterialAnm* m_anm;         // _3C
+	uint _40;                      // _40
+	uint _44;                      // _44
+	J3DDisplayListObj* _48;        // _48
+};
+
+/**
+ * m_*Block are all the *Null variants of their respective types.
+ * @size{0x4C}
+ */
+struct J3DLockedMaterial : public J3DMaterial {
+	virtual void calc(const float (*)[4]); // _00
+	virtual void makeDisplayList();        // _08
+	virtual void makeSharedDisplayList();  // _0C
+	virtual void load();                   // _10
+	virtual void loadSharedDL();           // _14
+	virtual void patch();                  // _18
+	virtual void diff(ulong);              // _1C
+	virtual void reset();                  // _20
+	virtual void change();                 // _24
+
+	void initialize();
+};
+
+/**
+ * m_texGenBlock and m_tevBlock are the *Patched variants of their respective
+ * types.
+ * @size{0x4C}
+ */
+struct J3DPatchedMaterial : public J3DMaterial {
+	virtual void makeDisplayList();       // _08
+	virtual void makeSharedDisplayList(); // _0C
+	virtual void load();                  // _10
+	virtual void loadSharedDL();          // _14
+	virtual void reset();                 // _20
+	virtual void change();                // _24
+
+	void initialize();
+};
+
+#endif
---
+++ include/JSystem/J3D/J3DTypes.h
@@ -0,0 +1,12 @@
+#ifndef _JSYSTEM_J3D_J3DTYPES_H
+#define _JSYSTEM_J3D_J3DTYPES_H
+
+#include "types.h"
+
+// TODO: Make a bitmask enum
+/**
+ * @fabricatedName
+ */
+typedef uint J3DModelLoaderFlags;
+
+#endif
