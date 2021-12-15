529a3864249be7b1a69cad932729f8fa630ba055
intns <84647527+intns@users.noreply.github.com>
2021-12-15 11:34:28 +0000

    Fix Animator.h

--- include/SysShape/Animator.h
+++ include/SysShape/Animator.h
@@ -11,12 +11,29 @@ struct AnimMgr;
 struct KeyEvent;
 
 struct BaseAnimator {
+	BaseAnimator() { }
+
 	virtual void getCalc()      = 0; // _00
 	virtual void animate(float) = 0; // _04
+
+	SysShape::MotionListener* m_listener; // _04
+	f32 m_timer;                          // _08
+	SysShape::AnimInfo* m_animInfo;       // _0C
+	SysShape::AnimMgr* m_animMgr;         // _10
+	SysShape::KeyEvent* m_curAnimKey;     // _14
+	u8 m_flags;                           // _18
 };
 
 struct Animator : public BaseAnimator {
-	// Animator();
+	Animator()
+	    : BaseAnimator()
+	{
+		m_flags    = 0;
+		m_animInfo = nullptr;
+		m_listener = nullptr;
+		m_flags    = 0;
+		m_animMgr  = nullptr;
+	}
 
 	virtual void getCalc();      // _00
 	virtual void animate(float); // _04
@@ -27,15 +44,7 @@ struct Animator : public BaseAnimator {
 	void setLastFrame();
 	void startAnim(int, SysShape::MotionListener*);
 	void startExAnim(SysShape::AnimInfo*);
-
-	SysShape::MotionListener* m_listener; // _04
-	f32 m_timer;                          // _08
-	SysShape::AnimInfo* m_animInfo;       // _0C
-	SysShape::AnimMgr* m_animMgr;         // _10
-	SysShape::KeyEvent* m_curAnimKey;     // _14
-	u8 m_flags;                           // _18
 };
-
 } // namespace SysShape
 
 #endif
