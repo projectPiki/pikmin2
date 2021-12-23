67db1941fdcf9bcae21c15a58032b5ceadd61b9e
intns <84647527+intns@users.noreply.github.com>
2021-12-23 17:40:32 +0000

    efx/TSimple.h

---
+++ include/efx/TSimple.h
@@ -0,0 +1,48 @@
+#ifndef _EFX_TSIMPLE_H
+#define _EFX_TSIMPLE_H
+
+struct Arg;
+
+namespace efx {
+struct TSimple1 {
+	virtual void create(Arg*); // _00
+	virtual void forceKill();  // _04
+	virtual void fade();       // _08
+
+	// _00 VTBL
+};
+
+struct TSimple2 {
+	virtual void create(Arg*); // _00
+	virtual void forceKill();  // _04
+	virtual void fade();       // _08
+
+	// _00 VTBL
+};
+
+struct TSimple3 {
+	virtual void create(Arg*); // _00
+	virtual void forceKill();  // _04
+	virtual void fade();       // _08
+
+	// _00 VTBL
+};
+
+struct TSimple4 {
+	virtual void create(Arg*); // _00
+	virtual void forceKill();  // _04
+	virtual void fade();       // _08
+
+	// _00 VTBL
+};
+
+struct TSimple5 {
+	virtual void create(Arg*); // _00
+	virtual void forceKill();  // _04
+	virtual void fade();       // _08
+
+	// _00 VTBL
+};
+} // namespace efx
+
+#endif
