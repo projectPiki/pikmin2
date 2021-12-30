8fc381ec5cfaa144289756e7058eebb0cdc1ef40
intns <84647527+intns@users.noreply.github.com>
2021-12-30 03:40:56 +0000

    Add DC, IC and LC functions

--- include/Dolphin/os.h
+++ include/Dolphin/os.h
@@ -92,6 +92,31 @@ typedef struct OSFstEntry {
 	char* m_fileNameMaybe;
 } OSFstEntry;
 
+void DCInvalidateRange(void* addr, u32 nBytes);
+void DCFlushRange(void* addr, u32 nBytes);
+void DCStoreRange(void* addr, u32 nBytes);
+void DCFlushRangeNoSync(void* addr, u32 nBytes);
+void DCStoreRangeNoSync(void* addr, u32 nBytes);
+void DCZeroRange(void* addr, u32 nBytes);
+void DCTouchRange(void* addr, u32 nBytes);
+
+void ICInvalidateRange(void* addr, u32 nBytes);
+
+#define LC_BASE_PREFIX 0xE000
+#define LC_BASE        (LC_BASE_PREFIX << 16)
+
+void LCEnable(void);
+void LCDisable(void);
+void LCLoadBlocks(void* destTag, void* srcAddr, u32 numBlocks);
+void LCStoreBlocks(void* destAddr, void* srcTag, u32 numBlocks);
+u32 LCLoadData(void* destAddr, void* srcAddr, u32 nBytes);
+u32 LCStoreData(void* destAddr, void* srcAddr, u32 nBytes);
+u32 LCQueueLength(void);
+void LCQueueWait(u32 len);
+void LCFlushQueue(void);
+
+#define LCGetBase() ((void*)LC_BASE)
+
 #ifdef __cplusplus
 };
 #endif // ifdef __cplusplus
