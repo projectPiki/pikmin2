e3a65cd910c32c1b23eae8fe3ea5ec92b4dfd9bd
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-01-03 17:58:54 -0500

    JStudio Object header

--- include/JStudio/object.h
+++ include/JStudio/object.h
@@ -1,12 +1,23 @@
 #ifndef JSTUDIO_OBJECT_H
 #define JSTUDIO_OBJECT_H
 
-namespace JStudio::object {
+#include "types.h"
 
-struct TPRObject_ID_equal {
-	int* value;
-};
+namespace JStudio {
+namespace object {
 
-} // namespace JStudio::object
+	struct TIDData {
+		u32* idString_0x0;
+		u32 lengthInBytes_0x4;
+
+		bool isEqual(TIDData const&, TIDData const&);
+	};
+
+	struct TPRObject_ID_equal {
+		int* value;
+	};
+
+} // namespace object
+} // namespace JStudio
 
 #endif
