10dbfb786ec64cac3d5569050bc0e2655dac1b8c
intns <84647527+intns@users.noreply.github.com>
2022-01-04 12:10:21 +0000

    Remove useless semicolons in tagparams_jank

--- src/sysCommonU/tagparams_jank.cpp
+++ src/sysCommonU/tagparams_jank.cpp
@@ -7,18 +7,18 @@
  * Address:	8041BD2C
  * Size:	000004
  */
-void TagParm::doRead(Stream&) {};
+void TagParm::doRead(Stream&) {}
 
 /*
  * --INFO--
  * Address:	8041BD30
  * Size:	000004
  */
-void TagParm::doWrite(Stream&) {};
+void TagParm::doWrite(Stream&) {}
 
 /*
  * --INFO--
  * Address:	8041BD34
  * Size:	000004
  */
-void TagParm::doDump() {};
+void TagParm::doDump() {}
