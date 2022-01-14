d6743fdb4b260ae362eb2fb705f2e82e7ef044c6
SodiumChlorideLogic <19625889+SodiumChlorideLogic@users.noreply.github.com>
2022-01-13 21:46:12 -0500

    Condense Sys::Mat*Anim* classes into 2 headers. Also fill them out a bit.

--- include/Sys/MatBaseAnimation.h
+++ include/Sys/MatBaseAnimation.h
@@ -1,10 +1,55 @@
 #ifndef _SYS_MATBASEANIMATION_H
 #define _SYS_MATBASEANIMATION_H
 
+#include "JSystem/J3D/J3DAnmBase.h"
+#include "JSystem/J3D/J3DModel.h"
+#include "JSystem/J3D/J3DTevColorAnm.h"
+
 namespace Sys {
 struct MatBaseAnimation {
+	virtual void onAttachResource(void*) = 0; // _00
+	virtual J3DAnmBase* getAnmBase()     = 0; // _04
+	virtual void set()                   = 0; // _08
+	virtual bool remove()                = 0; // _0C
+
+	// virtual void _10() = 0; // _10
+
 	void attachResource(void*, struct J3DModelData*);
 	void getFrameMax();
+
+	// VTBL _00
+	J3DModelData* m_modelData; // _04
+};
+
+/**
+ * @size{0x14}
+ */
+struct MatTevRegAnimation : MatBaseAnimation {
+	MatTevRegAnimation();
+
+	virtual void onAttachResource(void*); // _00
+	virtual J3DAnmBase* getAnmBase();     // _04
+	virtual void set();                   // _08
+	virtual bool remove();                // _0C
+
+	J3DAnmTevRegKey* m_anmTevRegKey; // _08
+	J3DTevColorAnm* m_tevColorAnm;   // _10
+	J3DTevKColorAnm* m_tevKColorAnm; // _14
+};
+
+/**
+ * @size{0x10}
+ */
+struct MatTexAnimation : MatBaseAnimation {
+	MatTexAnimation();
+
+	virtual void onAttachResource(void*); // _00
+	virtual J3DAnmBase* getAnmBase();     // _04
+	virtual void set();                   // _08
+	virtual bool remove();                // _0C
+
+	J3DAnmTextureSRTKey* _08; // _08
+	u32 _0C;
 };
 } // namespace Sys
 
--- include/Sys/MatBaseAnimator.h
+++ include/Sys/MatBaseAnimator.h
@@ -1,17 +1,42 @@
 #ifndef _SYS_MATBASEANIMATOR_H
 #define _SYS_MATBASEANIMATOR_H
 
+#include "types.h"
 namespace Sys {
 struct MatBaseAnimation;
 
 struct MatBaseAnimator {
 	MatBaseAnimator();
 
+	virtual void start(Sys::MatBaseAnimation*); // _00
+	virtual void onStart();                     // _04
+	virtual void do_animate(float);             // _08
+
 	void animate(float);
-	void do_animate(float);
-	void onStart();
 	void setCurrentFrame(float);
-	void start(Sys::MatBaseAnimation*);
+
+	// VTBL _00
+	MatBaseAnimation* m_animation; // _04
+	float _08;                     // _08
+};
+
+/**
+ * @size{0xC}
+ */
+struct MatLoopAnimator : MatBaseAnimator {
+	MatLoopAnimator();
+
+	virtual void do_animate(float); // _08
+};
+
+/**
+ * @size{0x10}
+ */
+struct MatRepeatAnimator : MatBaseAnimator {
+	virtual void onStart();         // _04
+	virtual void do_animate(float); // _08
+
+	u8 _0C; // _0C
 };
 } // namespace Sys
 
