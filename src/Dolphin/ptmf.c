b42109fbcd262b33d3cc809f1ac8271405785a12
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-01-24 18:35:11 -0500

    formatting

--- src/Dolphin/ptmf.c
+++ src/Dolphin/ptmf.c
@@ -75,16 +75,16 @@ void __ptmf_call4(void)
 asm void __ptmf_scall(...)
 {
 	nofralloc
-		lwz		r0, PTMF.this_delta(r12)
-		lwz		r11, PTMF.v_offset(r12)
-		lwz		r12, PTMF.f_data(r12)
-		add		r3, r3, r0
-		cmpwi	r11, 0
-		blt-	cr0, loc_0x20
-		lwzx	r12, r3, r12
-		lwzx	r12, r12, r11
+		lwz     r0, PTMF.this_delta(r12)
+		lwz     r11, PTMF.v_offset(r12)
+		lwz     r12, PTMF.f_data(r12)
+		add     r3, r3, r0
+		cmpwi   r11, 0
+		blt-    cr0, loc_0x20
+		lwzx    r12, r3, r12
+		lwzx    r12, r12, r11
 	loc_0x20:
-		mtctr	r12
+		mtctr   r12
 		bctr
 }
 // clang-format on
