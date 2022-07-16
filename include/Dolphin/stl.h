61fea041de9e00ae7ea83856518c4d0783c4e317
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-07-16 13:44:09 -0400

    stream.cpp (#23)
    * RamStream functions and writeShort
    
    thank you encounter, legend
    
    * va_arg fixes
    
    * stream.h matched
    
    still has linking errors that need fixing to do with Stream's vtable being called by RamStream::RamStream
    
    Co-Authored-By: Luke Street <luke@street.dev>
    
    * va_arg updates/preliminary linking
    
    NON-MATCHING
    
    * text fixes
    
    * stream.cpp :ok:
    
    2%
    
    * formatting
    
    Co-authored-by: HeartPiece <deancsouthwood@gmail.com>
    Co-authored-by: Luke Street <luke@street.dev>

--- include/Dolphin/stl.h
+++ include/Dolphin/stl.h
@@ -13,18 +13,46 @@ __declspec(section ".init") void* memcpy(void*, const void*, size_t);
 __declspec(section ".init") void __fill_mem(void*, int, size_t);
 __declspec(section ".init") void* memset(void*, int, size_t);
 
-typedef struct __va_list_struct {
+// typedef struct __va_list_struct {
+// 	char gpr;
+// 	char fpr;
+// 	char* input_arg_area;
+// 	char* reg_save_area;
+// } va_list[1];
+
+// void* __va_arg(va_list, int);
+
+// #define va_start(ARG, VA_LIST)    ((void)ARG, __builtin_va_info(&VA_LIST))
+// #define va_end(VA_LIST)           ((void)VA_LIST)
+// #define va_arg(VA_LIST, ARG_TYPE) (*(ARG_TYPE*)) __va_arg(VA_LIST, _var_arg_typeof(ARG_TYPE))
+
+#ifdef __MWERKS__
+typedef struct
+{
 	char gpr;
 	char fpr;
-	char* input_arg_area;
-	char* reg_save_area;
-} va_list[1];
+	char reserved[2];
+	char *input_arg_area;
+	char *reg_save_area;
+} __va_list[1];
+typedef __va_list va_list;
 
-void* __va_arg(va_list, int);
+#ifndef __MWERKS__
+extern void __builtin_va_info(va_list *);
+#endif
 
-#define va_start(ARG, VA_LIST)    ((void)ARG, __builtin_va_info(&VA_LIST))
-#define va_end(VA_LIST)           ((void)VA_LIST)
-#define va_arg(VA_LIST, ARG_TYPE) (*(ARG_TYPE*)) __va_arg(VA_LIST, _var_arg_typeof(ARG_TYPE))
+void *__va_arg(va_list v_list, unsigned char type);
+
+#define va_start(ap,fmt) ((void)fmt,__builtin_va_info(&ap))
+#define va_arg(ap,t)     (*((t*)__va_arg(ap,_var_arg_typeof(t))))
+#define va_end(ap)       (void)0
+
+#else
+typedef __builtin_va_list va_list;
+#define va_start(v,l) __builtin_va_start(v,l)
+#define va_end(v)     __builtin_va_end(v)
+#define va_arg(v,l)	  __builtin_va_arg(v,l)
+#endif
 
 /*
  * --INFO--
@@ -39,6 +67,7 @@ int vprintf(const char*, va_list);
 int sprintf(char*, char*, ...);
 int snprintf(char*, size_t, const char*, ...);
 int vsnprintf(char*, size_t, const char*, va_list);
+int vsprintf(char* s, const char* format, va_list arg);
 
 int rand();
 
