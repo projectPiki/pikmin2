687a011fe73934c7d4fe4a21e0cd8543edbe745e
Luke Street <luke.street@encounterpc.com>
2022-07-18 13:21:04 -0400

    Fix ogSceneChallengeBase vtable

--- obj_files.mk
+++ obj_files.mk
@@ -936,7 +936,6 @@ OGAWA:=\
 	$(BUILD_DIR)/asm/plugProjectOgawaU/ogSceneWorldMapInfoWindow1.o\
 	$(BUILD_DIR)/asm/plugProjectOgawaU/ogTotalPokoScreen.o\
 	$(BUILD_DIR)/asm/plugProjectOgawaU/ogObjChallengeBase.o\
-	$(BUILD_DIR)/asm/plugProjectOgawaU/ogSceneChallengeBase.o\
 	$(BUILD_DIR)/src/plugProjectOgawaU/ogSceneChallengeBase.o\
 	$(BUILD_DIR)/asm/plugProjectOgawaU/ogCallBackPicture.o\
 	$(BUILD_DIR)/asm/plugProjectOgawaU/ogDrawAfter.o\
