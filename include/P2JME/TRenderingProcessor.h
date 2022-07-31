4b2d1c87ca3eaa4529cadb8743b39c477fbc0671
HeartPiece <deancsouthwood@gmail.com>
2022-07-31 23:52:37 +1000

    Update TRenderingProcessor.h

--- include/P2JME/TRenderingProcessor.h
+++ include/P2JME/TRenderingProcessor.h
@@ -6,26 +6,51 @@
 #include "types.h"
 
 struct JUTFont;
+struct JUTTexture;
 
 namespace P2JME {
 
 struct TRenderingProcessorBase : JMessage::TRenderingProcessor {
-	virtual ~TRenderingProcessorBase();                                   // _08 (weak)
-	virtual void do_character(int);                                       // _10 (weak)
-	virtual void do_tag(unsigned long, const void*, unsigned long);       // _14
-	virtual void tagColor(const void*, unsigned long);                    // _48 (weak)
-	virtual void tagSize(const void*, unsigned long);                     // _4C (weak)
-	virtual void tagRuby(const void*, unsigned long);                     // _50 (weak)
-	virtual void tagFont(const void*, unsigned long);                     // _54 (weak)
-	virtual void tagImage(unsigned short, const void*, unsigned long);    // _58 (weak)
-	virtual void tagColorEX(unsigned short, const void*, unsigned long);  // _5C (weak)
-	virtual void tagControl(unsigned short, const void*, unsigned long);  // _60 (weak)
-	virtual void tagPosition(unsigned short, const void*, unsigned long); // _64 (weak)
+	// virtual ~TRenderingProcessorBase();                                   // _08 (weak)
+	// virtual void do_character(int);                                       // _10 (weak)
+	// virtual void do_tag(unsigned long, const void*, unsigned long);       // _14
+	// virtual void tagColor(const void*, unsigned long);                    // _48 (weak)
+	// virtual void tagSize(const void*, unsigned long);                     // _4C (weak)
+	// virtual void tagRuby(const void*, unsigned long);                     // _50 (weak)
+	// virtual void tagFont(const void*, unsigned long);                     // _54 (weak)
+	// virtual void tagImage(unsigned short, const void*, unsigned long);    // _58 (weak)
+	// virtual void tagColorEX(unsigned short, const void*, unsigned long);  // _5C (weak)
+	// virtual void tagControl(unsigned short, const void*, unsigned long);  // _60 (weak)
+	// virtual void tagPosition(unsigned short, const void*, unsigned long); // _64 (weak)
 
 	TRenderingProcessorBase(const JMessage::TReference*);
 };
 
-struct TRenderingProcessor : JMessage::TRenderingProcessor {
+struct TRenderingProcessor : TRenderingProcessorBase {
+	// virtual ~TRenderingProcessor();                                             // _08
+	// virtual void do_character(int);                                             // _10
+	// virtual void do_tag(unsigned long, const void*, unsigned long);             // _14
+	// virtual void do_systemTagCode(unsigned short, const void*, unsigned long);  // _18
+	// virtual void do_select_begin(unsigned long);                                // _1C
+	// virtual void do_select_end();                                               // _20
+	// virtual void do_select_separate();                                          // _24
+	// virtual void do_begin(const void*, const char*);                            // _40
+	// virtual void tagColor(const void*, unsigned long);                    		// _48 (weak)
+	// virtual void tagSize(const void*, unsigned long);                     		// _4C (weak)
+	// virtual void tagRuby(const void*, unsigned long);                     		// _50 (weak)
+	// virtual void tagFont(const void*, unsigned long);                     		// _54 (weak)
+	// virtual void tagImage(unsigned short, const void*, unsigned long);    		// _58 (weak)
+	// virtual void tagColorEX(unsigned short, const void*, unsigned long);  		// _5C (weak)
+	// virtual void tagControl(unsigned short, const void*, unsigned long);  		// _60 (weak)
+	// virtual void tagPosition(unsigned short, const void*, unsigned long); 		// _64 (weak)
+	// virtual void update();														// _68 (weak)
+	// virtual void reset();														// _6C
+	// virtual void newParagraph();												// _70
+	// virtual void doDrawImage(JUTTexture*, float, float, float, float);			// _74
+	// virtual void doDrawRuby(float, float, float, float, int, bool);				// _78
+	// virtual void doDrawLetter(float, float, float, float, int, bool);			// _7C
+	// virtual void doTagControlAbtnWait();										// _80 (weak)
+
 	TRenderingProcessor(JMessage::TReference const*);
 
 	void init();
