215f6916f10c481d384ae1ff5150cdffeec1af6b
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-08-22 19:18:00 -0400

    Update PlayData.h

--- include/Game/PlayData.h
+++ include/Game/PlayData.h
@@ -1,13 +1,15 @@
 #ifndef _GAME_PLAYDATA_H
 #define _GAME_PLAYDATA_H
 #include "types.h"
-
+namespace Game {
 typedef struct PlayData;
 struct PlayData {
 	bool hasDope(int);
 	int getDopeCount(int);
 	void useDope(int);
+	int getCurrentCourseIndex();
 };
 extern PlayData* playData;
+} // namespace Game
 
 #endif
