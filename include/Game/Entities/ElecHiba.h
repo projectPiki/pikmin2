03aadbde34a2639b7fa7a6cf8c7dcc338a1b6462
HeartPiece <deancsouthwood@gmail.com>
2022-09-25 17:59:48 +1000

    Match + link ElecHibaState

--- include/Game/Entities/ElecHiba.h
+++ include/Game/Entities/ElecHiba.h
@@ -36,20 +36,23 @@ struct Obj : public EnemyBase {
 	Obj();
 
 	//////////////// VTABLE
-	virtual void onInit(CreatureInitArg*);                   // _30
-	virtual void doSimulation(f32);                          // _4C (weak)
-	virtual void doDirectDraw(Graphics&);                    // _50
-	virtual void inWaterCallback(WaterBox*);                 // _84 (weak)
-	virtual void outWaterCallback();                         // _88 (weak)
-	virtual bool isLivingThing();                            // _D4 (weak)
-	virtual void getShadowParam(ShadowParam&);               // _134
-	virtual ~Obj();                                          // _1BC (weak)
-	virtual void birth(Vector3f&, f32);                      // _1C0
-	virtual void setInitialSetting(EnemyInitialParamBase*);  // _1C4
-	virtual void doUpdate();                                 // _1CC
-	virtual void doDebugDraw(Graphics&);                     // _1EC
-	virtual bool injure();                                   // _23C
-	virtual EnemyTypeID::EEnemyTypeID getEnemyTypeID();      // _258 (weak)
+	virtual void onInit(CreatureInitArg*);                  // _30
+	virtual void doSimulation(f32);                         // _4C (weak)
+	virtual void doDirectDraw(Graphics&);                   // _50
+	virtual void inWaterCallback(WaterBox*);                // _84 (weak)
+	virtual void outWaterCallback();                        // _88 (weak)
+	virtual bool isLivingThing();                           // _D4 (weak)
+	virtual void getShadowParam(ShadowParam&);              // _134
+	virtual ~Obj();                                         // _1BC (weak)
+	virtual void birth(Vector3f&, f32);                     // _1C0
+	virtual void setInitialSetting(EnemyInitialParamBase*); // _1C4
+	virtual void doUpdate();                                // _1CC
+	virtual void doDebugDraw(Graphics&);                    // _1EC
+	virtual bool injure();                                  // _23C
+	virtual EnemyTypeID::EEnemyTypeID getEnemyTypeID()      // _258 (weak)
+	{
+		return EnemyTypeID::EnemyID_ElecHiba;
+	}
 	virtual void doGetLifeGaugeParam(LifeGaugeParam&);       // _260
 	virtual bool damageCallBack(Creature*, f32, CollPart*);  // _278
 	virtual bool pressCallBack(Creature*, f32, CollPart*);   // _27C
@@ -59,7 +62,7 @@ struct Obj : public EnemyBase {
 	virtual void setFSM(FSM*);                               // _2F8
 	//////////////// VTABLE END
 
-	void getChildObjPtr();
+	Obj* getChildObjPtr();
 	void setElecHibaPosition(InitialParam*, f32);
 	void interactDenkiAttack(Vector3f&);
 	void addDamageMyself(f32);
@@ -76,17 +79,19 @@ struct Obj : public EnemyBase {
 	void setVersusHibaType();
 	void resetAttrHitCount();
 	void addAttrAttackCount(Piki*);
-	void isWaitFinish();
-	void isAttackFinish();
+	bool isWaitFinish();
+	bool isAttackFinish();
 
 	// _00 		= VTBL
 	// _00-_2BC	= EnemyBase
 	FSM* m_FSM;                            // _2BC
-	u8 _2C0[0x8];                          // _2C0, unknown
+	u8 _2C0;                               // _2C0
+	f32 m_waitTimer;                       // _2C4
 	TeamList m_teamList;                   // _2C8
 	u8 _2E0[0x10];                         // _2E0, unknown
 	efx::TDenkiHibaMgr* m_efxDenkiHibaMgr; // _2F0
-	u8 _2F4[0x10];                         // _2F4, unknown
+	u8 _2F4;                               // _2F4, might be a bool?
+	u8 _2F5[0x9];                          // _2F5, unknown
 	                                       // _304 = PelletView
 };
 
@@ -154,6 +159,14 @@ struct ProperAnimator : public EnemyAnimatorBase {
 
 /////////////////////////////////////////////////////////////////
 // STATE MACHINE DEFINITIONS
+enum StateID {
+	ELECHIBA_Dead   = 0,
+	ELECHIBA_Wait   = 1,
+	ELECHIBA_Sign   = 2,
+	ELECHIBA_Attack = 3,
+	ELECHIBA_Count  = 4,
+};
+
 struct FSM : public EnemyStateMachine {
 	virtual void init(EnemyBase*); // _08
 
@@ -162,11 +175,22 @@ struct FSM : public EnemyStateMachine {
 };
 
 struct State : public EnemyFSMState {
+	inline State(u16 stateID, const char* name)
+	    : EnemyFSMState(stateID)
+	{
+		m_name = name;
+	}
+
 	// _00		= VTBL
 	// _00-_10 	= EnemyFSMState
 };
 
 struct StateAttack : public State {
+	inline StateAttack()
+	    : State(ELECHIBA_Attack, "attack")
+	{
+	}
+
 	virtual void init(EnemyBase*, StateArg*); // _08
 	virtual void exec(EnemyBase*);            // _0C
 	virtual void cleanup(EnemyBase*);         // _10
@@ -176,6 +200,11 @@ struct StateAttack : public State {
 };
 
 struct StateDead : public State {
+	inline StateDead()
+	    : State(ELECHIBA_Dead, "dead")
+	{
+	}
+
 	virtual void init(EnemyBase*, StateArg*); // _08
 	virtual void exec(EnemyBase*);            // _0C
 	virtual void cleanup(EnemyBase*);         // _10
@@ -185,6 +214,11 @@ struct StateDead : public State {
 };
 
 struct StateSign : public State {
+	inline StateSign()
+	    : State(ELECHIBA_Sign, "sign")
+	{
+	}
+
 	virtual void init(EnemyBase*, StateArg*); // _08
 	virtual void exec(EnemyBase*);            // _0C
 	virtual void cleanup(EnemyBase*);         // _10
@@ -194,6 +228,11 @@ struct StateSign : public State {
 };
 
 struct StateWait : public State {
+	inline StateWait()
+	    : State(ELECHIBA_Wait, "wait")
+	{
+	}
+
 	virtual void init(EnemyBase*, StateArg*); // _08
 	virtual void exec(EnemyBase*);            // _0C
 	virtual void cleanup(EnemyBase*);         // _10
