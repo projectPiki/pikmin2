234f45e11a95c8f0e9db5ab4070fb0f07ed9b23e
HeartPiece <deancsouthwood@gmail.com>
2022-10-09 14:27:49 +1100

    Update AmcExi2Stubs.c

--- src/Dolphin/AmcExi2Stubs.c
+++ src/Dolphin/AmcExi2Stubs.c
@@ -6,53 +6,53 @@
  * Address:	800D2644
  * Size:	000004
  */
-void EXI2_Init(void) { return; }
+void EXI2_Init(s32 a, s32 b) { return; }
 
 /*
  * --INFO--
  * Address:	800D2648
  * Size:	000004
  */
-void EXI2_EnableInterrupts(void) { return; }
+void EXI2_EnableInterrupts() { return; }
 
 /*
  * --INFO--
  * Address:	800D264C
  * Size:	000008
  */
-BOOL EXI2_Poll(void) { return FALSE; }
+BOOL EXI2_Poll() { return FALSE; }
 
 /*
  * --INFO--
  * Address:	800D2654
  * Size:	000008
  */
-BOOL EXI2_ReadN(u8*, int) { return FALSE; }
+BOOL EXI2_ReadN(u8* a, int b) { return FALSE; }
 
 /*
  * --INFO--
  * Address:	800D265C
  * Size:	000008
  */
-BOOL EXI2_WriteN(void) { return FALSE; }
+BOOL EXI2_WriteN(u32 a, u32 b) { return FALSE; }
 
 /*
  * --INFO--
  * Address:	800D2664
  * Size:	000004
  */
-void EXI2_Reserve(void) { return; }
+void EXI2_Reserve() { return; }
 
 /*
  * --INFO--
  * Address:	800D2668
  * Size:	000004
  */
-void EXI2_Unreserve(void) { return; }
+void EXI2_Unreserve() { return; }
 
 /*
  * --INFO--
  * Address:	800D266C
  * Size:	000008
  */
-BOOL AMC_IsStub(void) { return TRUE; }
+BOOL AMC_IsStub() { return TRUE; }
