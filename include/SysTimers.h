0929139ea6b06f8a4b647e0639dc51787affff0d
intns <84647527+intns@users.noreply.github.com>
2022-10-20 02:35:57 +0100

    sysTimers match but unable to link

--- include/SysTimers.h
+++ include/SysTimers.h
@@ -9,17 +9,31 @@
  * @size{0x3C}
  */
 struct TimerInf : public JKRDisposer {
+	TimerInf(char* name = "noname")
+	{
+		m_state = 0;
+
+		m_name = name;
+		_20    = 0.0f;
+		_24    = 0.0f;
+		_28    = 0.0f;
+		_2C    = 0.0f;
+
+		_30 = 0;
+		_34 = -1;
+	}
+
 	virtual ~TimerInf(); // _08 (weak)
 
 	char* m_name; // _18
-	u8 _1C[4];    // _1C
-	float _20;    // _20
-	float _24;    // _24
-	float _28;    // _28
-	float _2C;    // _2C
-	unknown _30;  // _30
-	int _34;      // _34
-	u8 _38;       // _38
+	u32 _1C;      // _1C
+	f32 _20;      // _20
+	f32 _24;      // _24
+	f32 _28;      // _28
+	f32 _2C;      // _2C
+	s32 _30;      // _30
+	s32 _34;      // _34
+	u8 m_state;   // _38
 };
 
 /**
@@ -27,24 +41,19 @@ struct TimerInf : public JKRDisposer {
  */
 struct SysTimers : public CNode {
 	SysTimers();
-
 	virtual ~SysTimers(); // _08 (weak)
 
-	TimerInf* get(int);
+	TimerInf* get(int index);
 	void reset();
 	void newFrame();
-	void remove(TimerInf*);
-	void _start(char*, bool);
-	void _stop(char*);
+	void remove(TimerInf* timer);
+	void _start(char* name, bool);
+	void _stop(char* name);
 
-	// Inlined/Unused:
-	void showTimes() {};
-	void draw(struct J2DGrafContext&) {};
-
-	int _18;                 // _18
-	int m_infoCount;         // _1C
-	struct TimerInf* m_info; // _20
-	float _24;               // _24
+	s32 m_frameCount;   // _18
+	s32 m_timerCount;   // _1C
+	TimerInf* m_timers; // _20
+	f32 _24;            // _24
 
 	static u8 drawFlag;
 };
--- src/sysGCU/sysTimers.cpp
+++ src/sysGCU/sysTimers.cpp
@@ -1,4 +1,3 @@
-#include "types.h"
 
 /*
     Generated from dpostproc
@@ -64,113 +63,34 @@
         .4byte 0x00000000
 */
 
+#include "JSystem/JUT/JUTException.h"
+
+#include "SysTimers.h"
+#include "System.h"
+
+u8 SysTimers::drawFlag;
+
 /*
  * --INFO--
  * Address:	8042A7FC
  * Size:	0000E0
  */
 SysTimers::SysTimers()
+    : m_frameCount(-1)
 {
-	/*
-	stwu     r1, -0x20(r1)
-	mflr     r0
-	stw      r0, 0x24(r1)
-	stw      r31, 0x1c(r1)
-	mr       r31, r3
-	stw      r30, 0x18(r1)
-	stw      r29, 0x14(r1)
-	bl       __ct__5CNodeFv
-	lis      r3, __vt__9SysTimers@ha
-	li       r4, -1
-	addi     r0, r3, __vt__9SysTimers@l
-	li       r3, 0
-	stw      r0, 0(r31)
-	li       r0, 0x40
-	stw      r4, 0x18(r31)
-	stb      r3, drawFlag__9SysTimers@sda21(r13)
-	stw      r0, 0x1c(r31)
-	lwz      r30, 0x1c(r31)
-	mulli    r3, r30, 0x3c
-	addi     r3, r3, 0x10
-	bl       __nwa__FUl
-	lis      r4, __defctor__8TimerInfFv@ha
-	lis      r5, __dt__8TimerInfFv@ha
-	addi     r4, r4, __defctor__8TimerInfFv@l
-	mr       r7, r30
-	addi     r5, r5, __dt__8TimerInfFv@l
-	li       r6, 0x3c
-	bl       __construct_new_array
-	stw      r3, 0x20(r31)
-	li       r29, 0
-	addi     r30, r2, lbl_80520588@sda21
-	b        lbl_8042A89C
+	m_frameCount = -1;
+	drawFlag     = 0;
 
-lbl_8042A87C:
-	mr       r3, r31
-	mr       r4, r29
-	bl       get__9SysTimersFi
-	lbz      r0, 0x38(r3)
-	addi     r29, r29, 1
-	ori      r0, r0, 1
-	stb      r0, 0x38(r3)
-	stw      r30, 0x18(r3)
+	m_timerCount = 64;
+	m_timers     = new TimerInf[m_timerCount];
+	for (int i = 0; i < m_timerCount; i++) {
+		TimerInf* inf = get(i);
+		inf->m_state |= 1;
+		inf->m_name = "noname";
+	}
 
-lbl_8042A89C:
-	lwz      r0, 0x1c(r31)
-	cmpw     r29, r0
-	blt      lbl_8042A87C
-	lfs      f0, lbl_80520590@sda21(r2)
-	lis      r3, lbl_80499E10@ha
-	addi     r0, r3, lbl_80499E10@l
-	mr       r3, r31
-	stfs     f0, 0x24(r31)
-	stw      r0, 0x14(r31)
-	lwz      r31, 0x1c(r1)
-	lwz      r30, 0x18(r1)
-	lwz      r29, 0x14(r1)
-	lwz      r0, 0x24(r1)
-	mtlr     r0
-	addi     r1, r1, 0x20
-	blr
-	*/
-}
-
-/*
- * --INFO--
- * Address:	8042A8DC
- * Size:	00006C
- */
-void TimerInf::__defctor()
-{
-	/*
-	stwu     r1, -0x10(r1)
-	mflr     r0
-	stw      r0, 0x14(r1)
-	stw      r31, 0xc(r1)
-	mr       r31, r3
-	bl       __ct__11JKRDisposerFv
-	lis      r3, __vt__8TimerInf@ha
-	li       r5, 0
-	addi     r0, r3, __vt__8TimerInf@l
-	addi     r4, r2, lbl_80520588@sda21
-	stw      r0, 0(r31)
-	li       r0, -1
-	lfs      f0, lbl_80520590@sda21(r2)
-	mr       r3, r31
-	stb      r5, 0x38(r31)
-	stw      r4, 0x18(r31)
-	stfs     f0, 0x20(r31)
-	stfs     f0, 0x24(r31)
-	stfs     f0, 0x28(r31)
-	stfs     f0, 0x2c(r31)
-	stw      r5, 0x30(r31)
-	stw      r0, 0x34(r31)
-	lwz      r31, 0xc(r1)
-	lwz      r0, 0x14(r1)
-	mtlr     r0
-	addi     r1, r1, 0x10
-	blr
-	*/
+	_24    = 0.0f;
+	m_name = "sysTimers";
 }
 
 /*
@@ -178,84 +98,17 @@ void TimerInf::__defctor()
  * Address:	8042A948
  * Size:	000068
  */
-SysTimers::~SysTimers()
-{
-	/*
-	stwu     r1, -0x10(r1)
-	mflr     r0
-	stw      r0, 0x14(r1)
-	stw      r31, 0xc(r1)
-	mr       r31, r4
-	stw      r30, 8(r1)
-	or.      r30, r3, r3
-	beq      lbl_8042A994
-	lis      r4, __vt__9SysTimers@ha
-	addi     r0, r4, __vt__9SysTimers@l
-	stw      r0, 0(r30)
-	bl       reset__9SysTimersFv
-	mr       r3, r30
-	li       r4, 0
-	bl       __dt__5CNodeFv
-	extsh.   r0, r31
-	ble      lbl_8042A994
-	mr       r3, r30
-	bl       __dl__FPv
-
-lbl_8042A994:
-	lwz      r0, 0x14(r1)
-	mr       r3, r30
-	lwz      r31, 0xc(r1)
-	lwz      r30, 8(r1)
-	mtlr     r0
-	addi     r1, r1, 0x10
-	blr
-	*/
-}
+SysTimers::~SysTimers() { reset(); }
 
 /*
  * --INFO--
  * Address:	8042A9B0
  * Size:	00007C
  */
-void SysTimers::get(int)
+TimerInf* SysTimers::get(int idx)
 {
-	/*
-	stwu     r1, -0x10(r1)
-	mflr     r0
-	stw      r0, 0x14(r1)
-	stw      r31, 0xc(r1)
-	or.      r31, r4, r4
-	stw      r30, 8(r1)
-	mr       r30, r3
-	li       r3, 0
-	blt      lbl_8042A9E4
-	lwz      r0, 0x1c(r30)
-	cmpw     r31, r0
-	bge      lbl_8042A9E4
-	li       r3, 1
-
-lbl_8042A9E4:
-	clrlwi.  r0, r3, 0x18
-	bne      lbl_8042AA08
-	lis      r3, lbl_80499E1C@ha
-	lis      r5, lbl_80499E2C@ha
-	addi     r3, r3, lbl_80499E1C@l
-	li       r4, 0x77
-	addi     r5, r5, lbl_80499E2C@l
-	crclr    6
-	bl       panic_f__12JUTExceptionFPCciPCce
-
-lbl_8042AA08:
-	mulli    r0, r31, 0x3c
-	lwz      r3, 0x20(r30)
-	add      r3, r3, r0
-	lwz      r31, 0xc(r1)
-	lwz      r30, 8(r1)
-	lwz      r0, 0x14(r1)
-	mtlr     r0
-	addi     r1, r1, 0x10
-	blr
-	*/
+	P2ASSERTBOUNDSLINE(119, 0, idx, m_timerCount);
+	return &m_timers[idx];
 }
 
 /*
@@ -263,97 +116,28 @@ lbl_8042AA08:
  * Address:	8042AA2C
  * Size:	000074
  */
-TimerInf::~TimerInf()
-{
-	/*
-	stwu     r1, -0x10(r1)
-	mflr     r0
-	stw      r0, 0x14(r1)
-	stw      r31, 0xc(r1)
-	mr       r31, r4
-	stw      r30, 8(r1)
-	or.      r30, r3, r3
-	beq      lbl_8042AA84
-	lis      r3, __vt__8TimerInf@ha
-	mr       r4, r30
-	addi     r0, r3, __vt__8TimerInf@l
-	stw      r0, 0(r30)
-	lwz      r3, sys@sda21(r13)
-	lwz      r3, 0x28(r3)
-	bl       remove__9SysTimersFP8TimerInf
-	mr       r3, r30
-	li       r4, 0
-	bl       __dt__11JKRDisposerFv
-	extsh.   r0, r31
-	ble      lbl_8042AA84
-	mr       r3, r30
-	bl       __dl__FPv
-
-lbl_8042AA84:
-	lwz      r0, 0x14(r1)
-	mr       r3, r30
-	lwz      r31, 0xc(r1)
-	lwz      r30, 8(r1)
-	mtlr     r0
-	addi     r1, r1, 0x10
-	blr
-	*/
-}
+TimerInf::~TimerInf() { sys->m_timers->remove(this); }
 
 /*
  * --INFO--
  * Address:	8042AAA0
  * Size:	000038
  */
-void SysTimers::reset()
-{
-	/*
-	stwu     r1, -0x10(r1)
-	mflr     r0
-	lis      r3, lbl_80499E1C@ha
-	lis      r5, lbl_80499E38@ha
-	stw      r0, 0x14(r1)
-	addi     r3, r3, lbl_80499E1C@l
-	li       r4, 0x88
-	addi     r5, r5, lbl_80499E38@l
-	crclr    6
-	bl       panic_f__12JUTExceptionFPCciPCce
-	lwz      r0, 0x14(r1)
-	mtlr     r0
-	addi     r1, r1, 0x10
-	blr
-	*/
-}
+void SysTimers::reset() { JUT_PANICLINE(136, "有りえないー　有りえない― ザマシメック！！\n"); }
 
 /*
  * --INFO--
  * Address:	8042AAD8
  * Size:	000010
  */
-void SysTimers::newFrame()
-{
-	/*
-	lwz      r4, 0x18(r3)
-	addi     r0, r4, 1
-	stw      r0, 0x18(r3)
-	blr
-	*/
-}
+void SysTimers::newFrame() { m_frameCount++; }
 
 /*
  * --INFO--
  * Address:	8042AAE8
  * Size:	000010
  */
-void SysTimers::remove(TimerInf*)
-{
-	/*
-	lbz      r0, 0x38(r4)
-	ori      r0, r0, 1
-	stb      r0, 0x38(r4)
-	blr
-	*/
-}
+void SysTimers::remove(TimerInf* timer) { timer->m_state |= 1; }
 
 /*
  * --INFO--
@@ -368,23 +152,3 @@ void SysTimers::_start(char*, bool) { }
  * Size:	000004
  */
 void SysTimers::_stop(char*) { }
-
-/*
- * --INFO--
- * Address:	........
- * Size:	000004
- */
-void SysTimers::showTimes()
-{
-	// UNUSED FUNCTION
-}
-
-/*
- * --INFO--
- * Address:	........
- * Size:	000004
- */
-void SysTimers::draw(J2DGrafContext&)
-{
-	// UNUSED FUNCTION
-}
