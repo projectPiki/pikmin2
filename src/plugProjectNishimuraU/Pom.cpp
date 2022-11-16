4143063e31356591971a7e76a0ccdb4bf2ad1ef0
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2022-11-16 13:45:41 -0500

    match Pom::Obj::changeMaterial
    juggled some registers

--- src/plugProjectNishimuraU/Pom.cpp
+++ src/plugProjectNishimuraU/Pom.cpp
@@ -97,91 +97,16 @@ void Obj::changeMaterial()
 	J3DModel* j3dModel      = m_model->m_j3dModel;
 	J3DModelData* modelData = j3dModel->m_modelData;
 
-	u16 nameIdx           = modelData->m_materialTable._0C->getIndex("hanabiral_v");
+	u16 nameIdx           = j3dModel->m_modelData->m_materialTable._0C->getIndex("hanabiral_v");
 	J3DMaterial* material = modelData->m_materialTable.m_materials1[nameIdx];
 	material->m_tevBlock->setTevColor(0, m_rgbColor);
 	j3dModel->calcMaterial();
 
 	for (u16 i = 0; i < modelData->m_materialTable.m_count1; i++) {
-		J3DMatPacket* packet  = &j3dModel->m_matPackets[i];
-		j3dSys.m_matPacket    = packet;
-		J3DMaterial* material = modelData->m_materialTable.m_materials1[i];
-		material->diff(packet->_2C->_34);
+		J3DMatPacket& packet = j3dModel->m_matPackets[i];
+		j3dSys.m_matPacket   = &j3dModel->m_matPackets[i]; // changing to &packet affects regs r29, r30, and r31
+		modelData->m_materialTable.m_materials1[i]->diff(packet._2C->_34);
 	}
-	/*
-	stwu     r1, -0x20(r1)
-	mflr     r0
-	stw      r0, 0x24(r1)
-	stw      r31, 0x1c(r1)
-	stw      r30, 0x18(r1)
-	stw      r29, 0x14(r1)
-	stw      r28, 0x10(r1)
-	mr       r28, r3
-	lis      r3, lbl_80484CD8@ha
-	lwz      r5, 0x174(r28)
-	addi     r4, r3, lbl_80484CD8@l
-	lwz      r31, 8(r5)
-	lwz      r29, 4(r31)
-	lwz      r3, 0x64(r29)
-	bl       getIndex__10JUTNameTabCFPCc
-	lwz      r4, 0x60(r29)
-	rlwinm   r3, r3, 2, 0xe, 0x1d
-	lha      r0, 0x2dc(r28)
-	addi     r5, r1, 8
-	lwzx     r3, r4, r3
-	li       r4, 0
-	sth      r0, 8(r1)
-	lha      r0, 0x2de(r28)
-	sth      r0, 0xa(r1)
-	lha      r0, 0x2e0(r28)
-	sth      r0, 0xc(r1)
-	lha      r0, 0x2e2(r28)
-	sth      r0, 0xe(r1)
-	lwz      r3, 0x2c(r3)
-	lwz      r12, 0(r3)
-	lwz      r12, 0x64(r12)
-	mtctr    r12
-	bctrl
-	mr       r3, r31
-	lwz      r12, 0(r31)
-	lwz      r12, 0x14(r12)
-	mtctr    r12
-	bctrl
-	lis      r3, j3dSys@ha
-	li       r28, 0
-	addi     r30, r3, j3dSys@l
-	b        lbl_80254B5C
-
-lbl_80254B24:
-	lwz      r4, 0xc0(r31)
-	rlwinm   r3, r28, 6, 0xa, 0x19
-	rlwinm   r0, r28, 2, 0xe, 0x1d
-	add      r4, r4, r3
-	stw      r4, 0x3c(r30)
-	lwz      r3, 0x60(r29)
-	lwz      r4, 0x2c(r4)
-	lwzx     r3, r3, r0
-	lwz      r4, 0x34(r4)
-	lwz      r12, 0(r3)
-	lwz      r12, 0x24(r12)
-	mtctr    r12
-	bctrl
-	addi     r28, r28, 1
-
-lbl_80254B5C:
-	lhz      r0, 0x5c(r29)
-	clrlwi   r3, r28, 0x10
-	cmplw    r3, r0
-	blt      lbl_80254B24
-	lwz      r0, 0x24(r1)
-	lwz      r31, 0x1c(r1)
-	lwz      r30, 0x18(r1)
-	lwz      r29, 0x14(r1)
-	lwz      r28, 0x10(r1)
-	mtlr     r0
-	addi     r1, r1, 0x20
-	blr
-	*/
 }
 
 /*
