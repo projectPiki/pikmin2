e0568da54722c5dd1224b90dc0e89026965c7079
SwareJonge <olaf23okken@gmail.com>
2023-01-24 13:12:51 +0100

    fix and link JKRDvdFile

--- obj_files.mk
+++ obj_files.mk
@@ -49,7 +49,7 @@ JSYSTEM:=\
 	$(BUILD_DIR)/asm/JSystem/JKR/JKRCompArchive.o\
 	$(BUILD_DIR)/src/JSystem/JKR/JKRDecomp.o\
 	$(BUILD_DIR)/src/JSystem/JKR/JKRDisposer.o\
-	$(BUILD_DIR)/asm/JSystem/JKR/JKRDvdFile.o\
+	$(BUILD_DIR)/src/JSystem/JKR/JKRDvdFile.o\
 	$(BUILD_DIR)/asm/JSystem/JKR/JKRDvdAramRipper.o\
 	$(BUILD_DIR)/asm/JSystem/JKR/JKRDvdArchive.o\
 	$(BUILD_DIR)/asm/JSystem/JKR/JKRDvdRipper.o\
