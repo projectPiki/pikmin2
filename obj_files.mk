50bd33f1f56c25f84f16cd0fcb7b17c01a7087ea
EpochFlame <82550703+EpochFlame@users.noreply.github.com>
2023-01-28 18:25:56 -0500

    tiptoe around wibo's failure
    wibo cannnot handle multiple linker instances generating libraries in the same folder

--- obj_files.mk
+++ obj_files.mk
@@ -5,25 +5,25 @@ GROUP_0_FILES:=\
 	$(BUILD_DIR)/asm/dtors.o\
 
 JSYSTEM:=\
-	$(BUILD_DIR)/src/JSystem.a\
+	$(BUILD_DIR)/src/JSystem/JSystem.a\
 
 DOLPHIN:=\
-	$(BUILD_DIR)/src/Dolphin.a\
+	$(BUILD_DIR)/src/Dolphin/Dolphin.a\
 
 PLUGPROJECT:=\
-	$(BUILD_DIR)/src/plugProjectYamashitaU.a\
-	$(BUILD_DIR)/src/plugProjectKandoU.a\
-	$(BUILD_DIR)/src/plugProjectNishimuraU.a\
-	$(BUILD_DIR)/src/plugProjectOgawaU.a\
-	$(BUILD_DIR)/src/plugProjectHikinoU.a\
-	$(BUILD_DIR)/src/plugProjectMorimuraU.a\
-	$(BUILD_DIR)/src/plugProjectEbisawaU.a\
-	$(BUILD_DIR)/src/plugProjectKonoU.a\
+	$(BUILD_DIR)/src/plugProjectYamashitaU/plugProjectYamashitaU.a\
+	$(BUILD_DIR)/src/plugProjectKandoU/plugProjectKandoU.a\
+	$(BUILD_DIR)/src/plugProjectNishimuraU/plugProjectNishimuraU.a\
+	$(BUILD_DIR)/src/plugProjectOgawaU/plugProjectOgawaU.a\
+	$(BUILD_DIR)/src/plugProjectHikinoU/plugProjectHikinoU.a\
+	$(BUILD_DIR)/src/plugProjectMorimuraU/plugProjectMorimuraU.a\
+	$(BUILD_DIR)/src/plugProjectEbisawaU/plugProjectEbisawaU.a\
+	$(BUILD_DIR)/src/plugProjectKonoU/plugProjectKonoU.a\
 
 SYS:=\
-	$(BUILD_DIR)/src/sysBootupU.a\
-	$(BUILD_DIR)/src/sysCommonU.a\
-	$(BUILD_DIR)/src/sysGCU.a\
+	$(BUILD_DIR)/src/sysBootupU/sysBootupU.a\
+	$(BUILD_DIR)/src/sysCommonU/sysCommonU.a\
+	$(BUILD_DIR)/src/sysGCU/sysGCU.a\
 
 UTILITY:=\
-	$(BUILD_DIR)/src/utilityU.a\
+	$(BUILD_DIR)/src/utilityU/utilityU.a\
