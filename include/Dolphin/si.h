556a282d8e126d6b634f82ceb25d28e5ed384ffa
Amber Brault <celestialamber1@gmail.com>
2023-02-03 13:43:41 -0500

    Almost match GBAXfer.c

---
+++ include/Dolphin/si.h
@@ -0,0 +1,13 @@
+#ifndef _DOLPHIN_SI_H
+#define _DOLPHIN_SI_H
+
+#include "types.h"
+#include "Dolphin/os.h"
+
+typedef void (*SICallback)(int chan, u32 sr, OSContext* context);
+
+
+BOOL SITransfer(int, void*, int, void*, u32, SICallback, u32, u32);
+u32 SIGetTypeAsync(u32 chanID, void*);
+
+#endif
