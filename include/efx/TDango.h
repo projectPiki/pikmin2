62e77ccce7b82333941ec3c41a9b1be4f2c4b7fb
Hat Kid <6624576+Hat-Kid@users.noreply.github.com>
2023-03-28 22:51:08 +0200

    match some of `DangoMushi.cpp`

--- include/efx/TDango.h
+++ include/efx/TDango.h
@@ -8,6 +8,10 @@
 
 namespace efx {
 struct TDangoAttack2 : public TChaseMtx {
+	inline TDangoAttack2()
+	    : TChaseMtx(PID_DangoAttack2, nullptr)
+	{
+	}
 	virtual ~TDangoAttack2(); // _48 (weak)
 
 	// _00      = VTBL
@@ -15,6 +19,11 @@ struct TDangoAttack2 : public TChaseMtx {
 };
 
 struct TDangoCrash : public TSimple2 {
+	inline TDangoCrash()
+	    : TSimple2(PID_DangoCrash_1, PID_DangoCrash_2)
+	{
+	}
+
 	virtual bool create(Arg*); // _08
 
 	// _00      = VTBL
@@ -22,26 +31,51 @@ struct TDangoCrash : public TSimple2 {
 };
 
 struct TDangoDamage : public TSimpleMtx1 {
+	inline TDangoDamage(Matrixf* mtx)
+	    : TSimpleMtx1(mtx, PID_DangoDamage)
+	{
+	}
+
 	// _00      = VTBL
 	// _00-_10  = TSimpleMtx1
 };
 
 struct TDangoDead : public TSimpleMtx2 {
+	inline TDangoDead(Matrixf* mat)
+	    : TSimpleMtx2(mat, PID_DangoDead_1, PID_DangoDead_2)
+	{
+	}
+
 	// _00      = VTBL
 	// _00-_14  = TSimpleMtx2
 };
 
 struct TDangoDeadSmoke : public TSimpleMtx1 {
+	inline TDangoDeadSmoke(Matrixf* mtx)
+	    : TSimpleMtx1(mtx, PID_DangoDeadSmoke)
+	{
+	}
+
 	// _00      = VTBL
 	// _00-_10  = TSimpleMtx1
 };
 
 struct TDangoFly : public TSimple3 {
+	inline TDangoFly()
+	    : TSimple3(PID_DangoFly_1, PID_DangoFly_2, PID_DangoFly_3)
+	{
+	}
+
 	// _00      = VTBL
 	// _00-_18  = TSimple3
 };
 
 struct TDangoRun : public TChasePos2 {
+	inline TDangoRun()
+	    : TChasePos2(nullptr, PID_DangoRun_1, PID_DangoRun_2)
+	{
+	}
+	
 	// _00      = VTBL
 	// _00-_2C  = TChasePos2
 };
@@ -54,6 +88,11 @@ struct TDangoTurn : public TSimple2 {
 };
 
 struct TDangoWallBreak : public TChaseMtx {
+	inline TDangoWallBreak()
+	    : TChaseMtx(PID_DangoWallBreak, nullptr)
+	{
+	}
+
 	virtual ~TDangoWallBreak(); // _48 (weak)
 
 	// _00      = VTBL
