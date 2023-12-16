593cb8bd245ec5425269e719143828df68d0d79e
intns <84647527+intns@users.noreply.github.com>
2023-12-16 21:11:27 +0000

    Cleanup and document ID32 entirely

--- include/id32.h
+++ include/id32.h
@@ -5,43 +5,177 @@
 #include "types.h"
 #include "stream.h"
 
+/**
+ * @class ID32
+ *
+ * @brief The ID32 class represents a 32-bit identifier.
+ *
+ * This class provides functionality to store and manipulate 32-bit identifiers.
+ * The identifier can be represented as a string or an integer.
+ *
+ * The class also provides methods to read and write the identifier from/to a stream,
+ * as well as printing and formatting operations.
+ *
+ * @note The maximum length of the string representation is 4 characters.
+ */
 class ID32 {
 private:
-	char mStr[5]; // _00
+	char mStringID[5]; // _00
+
 	union {
-		char strView[4];
-		u32 intView;
+		char mStrView[4];
+		u32 mIntView;
 	} mId; // _08
 
 	void updateString();
 
 public:
+	/**
+	 * @brief Default constructor.
+	 *
+	 * Initializes the ID32 object with an empty identifier.
+	 */
 	ID32();
-	ID32(u32);
 
+	/**
+	 * @brief Constructor with integer parameter.
+	 *
+	 * Initializes the ID32 object with the specified integer identifier.
+	 *
+	 * @param id The integer identifier.
+	 */
+	ID32(u32 id);
+
+	/**
+	 * @brief Assignment operator.
+	 *
+	 * Assigns the identifier from another ID32 object.
+	 *
+	 * @param other The ID32 object to assign from.
+	 * @return A reference to the assigned ID32 object.
+	 */
 	inline ID32& operator=(const ID32& other)
 	{
-		__copy(mStr, const_cast<char*>(other.mStr), 5);
+		__copy(mStringID, const_cast<char*>(other.mStringID), 5);
 		mId = other.mId;
 		return *this;
 	}
-	void operator=(u32);
-	bool operator==(u32);
-	bool operator!=(u32);
 
-	bool match(u32, char);
+	/**
+	 * @brief Assignment operator with integer parameter.
+	 *
+	 * Assigns the specified integer identifier to the ID32 object.
+	 *
+	 * @param id The integer identifier to assign.
+	 */
+	void operator=(u32 id);
+
+	/**
+	 * @brief Equality comparison operator with integer parameter.
+	 *
+	 * Checks if the ID32 object is equal to the specified integer identifier.
+	 *
+	 * @param id The integer identifier to compare.
+	 * @return True if the ID32 object is equal to the specified identifier, false otherwise.
+	 */
+	bool operator==(u32 id);
+
+	/**
+	 * @brief Inequality comparison operator with integer parameter.
+	 *
+	 * Checks if the ID32 object is not equal to the specified integer identifier.
+	 *
+	 * @param id The integer identifier to compare.
+	 * @return True if the ID32 object is not equal to the specified identifier, false otherwise.
+	 */
+	bool operator!=(u32 id);
+
+	/**
+	 * @brief Matches the ID32 object against an integer identifier with an exception.
+	 *
+	 * Checks if the ID32 object matches the specified integer identifier,
+	 * except for a specific character position.
+	 *
+	 * @param id The integer identifier to match.
+	 * @param exception The character position to exclude from the matching.
+	 * @return True if the ID32 object matches the specified identifier, false otherwise.
+	 */
+	bool match(u32 id, char exception);
+
+	/**
+	 * @brief Checks if the ID32 object represents the end-of-file identifier.
+	 *
+	 * @return True if the ID32 object represents the end-of-file identifier, false otherwise.
+	 */
 	bool isEof();
-	void setID(u32);
+
+	/**
+	 * @brief Sets the ID32 object to the specified integer identifier.
+	 *
+	 * @param id The integer identifier to set.
+	 */
+	void setID(u32 id);
+
+	/**
+	 * @brief Updates the string representation of the ID32 object.
+	 *
+	 * This method updates the string representation of the ID32 object
+	 * based on the current integer identifier.
+	 */
 	void updateID();
-	void read(Stream&);
-	void write(Stream&);
-	void sprint(char*);
+
+	/**
+	 * @brief Reads the ID32 object from a stream.
+	 *
+	 * @param stream The stream to read from.
+	 */
+	void read(Stream& stream);
+
+	/**
+	 * @brief Writes the ID32 object to a stream.
+	 *
+	 * @param stream The stream to write to.
+	 */
+	void write(Stream& stream);
+
+	/**
+	 * @brief Formats the ID32 object as a string.
+	 *
+	 * @param str The buffer to store the formatted string.
+	 */
+	void sprint(char* str);
+
+	/**
+	 * @brief Prints the ID32 object to the standard output.
+	 */
 	void print();
 
-	inline char* getStrID() { return mId.strView; }
-	inline u32 getID() { return mId.intView; }
-	inline char* getStr() { return mStr; }
+	/**
+	 * @brief Gets the string representation of the ID32 object.
+	 *
+	 * @return The string representation of the ID32 object.
+	 */
+	inline char* getStrID() { return mId.mStrView; }
 
+	/**
+	 * @brief Gets the integer representation of the ID32 object.
+	 *
+	 * @return The integer representation of the ID32 object.
+	 */
+	inline u32 getID() { return mId.mIntView; }
+
+	/**
+	 * @brief Gets the string identifier of the ID32 object.
+	 *
+	 * @return The string identifier of the ID32 object.
+	 */
+	inline char* getStr() { return mStringID; }
+
+	/**
+	 * @brief End-of-file identifier.
+	 *
+	 * This static member represents the end-of-file identifier.
+	 */
 	static ID32 eof; // BSS
 };
 
--- src/sysCommonU/id32.cpp
+++ src/sysCommonU/id32.cpp
@@ -1,11 +1,13 @@
 #include "id32.h"
 
+ID32 ID32::eof('_eof');
+
 /*
  * --INFO--
  * Address:	80413254
  * Size:	00001C
  */
-bool ID32::isEof() { return mId.intView == '_eof'; }
+bool ID32::isEof() { return mId.mIntView == '_eof'; }
 
 /*
  * --INFO--
@@ -28,19 +30,26 @@ ID32::ID32(u32 id) { setID(id); }
  */
 void ID32::setID(u32 set_id)
 {
-	mId.intView = set_id;
+	mId.mIntView = set_id;
 	updateString();
 }
 
+/**
+ * @brief Checks if the given ID matches the ID stored, with an exception for a specific character.
+ *
+ * @param _id The ID to be checked.
+ * @param exception The character that is exempted from the comparison.
+ * @return true if the ID matches the stored ID, false otherwise.
+ */
 /*
  * --INFO--
  * Address:	804132FC
  * Size:	0000B8
  */
-bool ID32::match(u32 _id, char exception)
+bool ID32::match(u32 id, char exception)
 {
-	char* p_id = reinterpret_cast<char*>(&_id);
-	char* c_id = reinterpret_cast<char*>(&mId.intView);
+	char* p_id = reinterpret_cast<char*>(&id);
+	char* c_id = reinterpret_cast<char*>(&mId.mIntView);
 
 	for (int i = 0; i < 4; i++) {
 		if (p_id[i] != exception && p_id[i] != c_id[i]) {
@@ -51,6 +60,9 @@ bool ID32::match(u32 _id, char exception)
 	return true;
 }
 
+/**
+ * @brief Updates the number ID using the string ID.
+ */
 /*
  * --INFO--
  * Address:	804133B4
@@ -59,7 +71,7 @@ bool ID32::match(u32 _id, char exception)
 void ID32::updateID()
 {
 	for (int i = 0; i < 4; i++) {
-		mId.strView[i] = mStr[i];
+		mId.mStrView[i] = mStringID[i];
 	}
 }
 
@@ -70,12 +82,12 @@ void ID32::updateID()
  */
 void ID32::updateString()
 {
-	char* c_id = reinterpret_cast<char*>(&mId.intView);
+	char* c_id = reinterpret_cast<char*>(&mId.mIntView);
 
 	for (int i = 0; i < 4; i++) {
-		mStr[i] = c_id[i];
+		mStringID[i] = c_id[i];
 	}
-	mStr[4] = '\0';
+	mStringID[4] = '\0';
 }
 
 /*
@@ -83,9 +95,9 @@ void ID32::updateString()
  * Address:	80413404
  * Size:	000030
  */
-void ID32::operator=(u32 _id)
+void ID32::operator=(u32 id)
 {
-	mId.intView = _id;
+	mId.mIntView = id;
 	updateString();
 }
 /*
@@ -93,15 +105,22 @@ void ID32::operator=(u32 _id)
  * Address:	80413434
  * Size:	000014
  */
-bool ID32::operator==(u32 target) { return mId.intView == target; }
+bool ID32::operator==(u32 id) { return mId.mIntView == id; }
 
 /*
  * --INFO--
  * Address:	80413448
  * Size:	000018
  */
-bool ID32::operator!=(u32 _id) { return mId.intView != _id; }
+bool ID32::operator!=(u32 id) { return mId.mIntView != id; }
 
+/**
+ * @brief Writes the ID32 object to the given stream.
+ *
+ * @note If in text mode, it converts to a string and writes in the format "{str}". Otherwise, 4 bytes are written.
+ *
+ * @param stream The stream to write the ID32 object to.
+ */
 /*
  * --INFO--
  * Address:	80413460
@@ -116,12 +135,19 @@ void ID32::write(Stream& stream)
 		return;
 	}
 
-	stream.writeByte(mId.strView[3]);
-	stream.writeByte(mId.strView[2]);
-	stream.writeByte(mId.strView[1]);
-	stream.writeByte(mId.strView[0]);
+	stream.writeByte(mId.mStrView[3]);
+	stream.writeByte(mId.mStrView[2]);
+	stream.writeByte(mId.mStrView[1]);
+	stream.writeByte(mId.mStrView[0]);
 }
 
+/**
+ * @brief Reads the ID32 data from the given stream.
+ *
+ * @note If in text mode, the function reads the next token to update the ID32 string. Otherwise, it reads four bytes.
+ *
+ * @param stream The stream to read from.
+ */
 /*
  * --INFO--
  * Address:	804134F0
@@ -130,19 +156,19 @@ void ID32::write(Stream& stream)
 void ID32::read(Stream& stream)
 {
 	if (stream.mMode == STREAM_MODE_TEXT) {
-		char* token    = stream.getNextToken();
-		mId.strView[3] = token[3];
-		mId.strView[2] = token[2];
-		mId.strView[1] = token[1];
-		mId.strView[0] = token[0];
+		char* token     = stream.getNextToken();
+		mId.mStrView[3] = token[3];
+		mId.mStrView[2] = token[2];
+		mId.mStrView[1] = token[1];
+		mId.mStrView[0] = token[0];
 		updateString();
 		return;
 	}
 
-	mId.strView[3] = stream.readByte();
-	mId.strView[2] = stream.readByte();
-	mId.strView[1] = stream.readByte();
-	mId.strView[0] = stream.readByte();
+	mId.mStrView[3] = stream.readByte();
+	mId.mStrView[2] = stream.readByte();
+	mId.mStrView[1] = stream.readByte();
+	mId.mStrView[0] = stream.readByte();
 	updateString();
 }
 
@@ -153,18 +179,23 @@ void ID32::read(Stream& stream)
  */
 void ID32::print() { }
 
+/**
+ * @brief Converts the ID32 value to a string representation.
+ *
+ * @note The resulting string will be null-terminated.
+ *
+ * @param output The character array to store the string representation.
+ */
 /*
  * --INFO--
  * Address:	804135E0
  * Size:	000038
  */
-void ID32::sprint(char* str)
+void ID32::sprint(char* output)
 {
-	str[0] = mId.intView >> 24;
-	str[1] = static_cast<u8>(mId.intView >> 16);
-	str[2] = static_cast<u8>(mId.intView >> 8);
-	str[3] = static_cast<u8>(mId.intView);
-	str[4] = '\0';
+	output[0] = mId.mIntView >> 24;
+	output[1] = static_cast<u8>(mId.mIntView >> 16);
+	output[2] = static_cast<u8>(mId.mIntView >> 8);
+	output[3] = static_cast<u8>(mId.mIntView);
+	output[4] = '\0';
 }
-// static initializer
-ID32 ID32::eof('_eof');
