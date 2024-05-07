cde2667cfac42438b28722ae158f024e1ec86467
PikHacker <evangrune@gmail.com>
2024-05-07 02:11:05 -0400

    fix some of ebiScreenFileSelect_Mgr

--- src/plugProjectEbisawaU/ebiScreenFileSelect_Mgr.cpp
+++ src/plugProjectEbisawaU/ebiScreenFileSelect_Mgr.cpp
@@ -64,39 +64,34 @@ void FSMState_SelectYesNo::do_init(TMgr* mgr, Game::StateArg* arg)
  */
 void FSMState_SelectYesNo::do_exec(TMgr* mgr)
 {
-	if (mIsSelected) {
-		goto close;
-	}
-
-	if (mgr->mController->isMoveUp()) {
-		if (!mYesNoState) {
-			mgr->mMainScreen.setYesNo(true);
-			mYesNoState = true;
-			PSSystem::spSysIF->playSystemSe(PSSE_SY_MENU_CURSOR, 0);
+	if (!mIsSelected) {
+		if (mgr->mController->isMoveUp()) {
+			if (!mYesNoState) {
+				mgr->mMainScreen.setYesNo(true);
+				mYesNoState = true;
+				PSSystem::spSysIF->playSystemSe(PSSE_SY_MENU_CURSOR, 0);
+			}
+		} else if (mgr->mController->isMoveDown()) {
+			if (mYesNoState == true) {
+				mgr->mMainScreen.setYesNo(false);
+				mYesNoState = false;
+				PSSystem::spSysIF->playSystemSe(PSSE_SY_MENU_CURSOR, 0);
+			}
 		}
-	} else if (mgr->mController->isMoveDown()) {
-		if (mYesNoState == true) {
-			mgr->mMainScreen.setYesNo(false);
+
+		u32 input = mgr->mController->getButtonDown();
+		if (input & Controller::PRESS_A) {
+			mIsSelected = true;
+			PSSystem::spSysIF->playSystemSe(PSSE_SY_MENU_DECIDE, 0);
+		} else if (input & Controller::PRESS_B) {
 			mYesNoState = false;
-			PSSystem::spSysIF->playSystemSe(PSSE_SY_MENU_CURSOR, 0);
+			mIsSelected = true;
+			PSSystem::spSysIF->playSystemSe(PSSE_SY_MENU_CANCEL, 0);
 		}
-	}
-
-	u32 input = mgr->mController->getButtonDown();
-	if (input & Controller::PRESS_A) {
-		mIsSelected = true;
-		PSSystem::spSysIF->playSystemSe(PSSE_SY_MENU_DECIDE, 0);
-	} else if (input & Controller::PRESS_B) {
-		mYesNoState = false;
-		mIsSelected = true;
-		PSSystem::spSysIF->playSystemSe(PSSE_SY_MENU_CANCEL, 0);
-	} else {
-	close:
-		if (!mClosedMsg) {
-			mClosedMsg = true;
-			do_decide(mgr);
-			mgr->mMainScreen.closeMSG();
-		}
+	} else if (!mClosedMsg) {
+		mClosedMsg = true;
+		do_decide(mgr);
+		mgr->mMainScreen.closeMSG();
 	}
 
 	if (mgr->mMainScreen.isFinishCloseMSG() && mIsSelected && mClosedMsg) {
@@ -106,164 +101,6 @@ void FSMState_SelectYesNo::do_exec(TMgr* mgr)
 			transit(mgr, mNextStateNo, nullptr);
 		}
 	}
-
-	/*
-	stwu     r1, -0x10(r1)
-	mflr     r0
-	stw      r0, 0x14(r1)
-	stw      r31, 0xc(r1)
-	mr       r31, r4
-	stw      r30, 8(r1)
-	mr       r30, r3
-	lbz      r0, 0x21(r3)
-	cmplwi   r0, 0
-	bne      lbl_803DEA80
-	lwz      r3, 0xc00(r31)
-	lwz      r0, 0x18(r3)
-	rlwinm.  r0, r0, 0, 0x1c, 0x1c
-	bne      lbl_803DE980
-	lfs      f1, 0x4c(r3)
-	lfs      f0, lbl_8051FC08@sda21(r2)
-	fcmpo    cr0, f1, f0
-	ble      lbl_803DE988
-
-lbl_803DE980:
-	li       r0, 1
-	b        lbl_803DE98C
-
-lbl_803DE988:
-	li       r0, 0
-
-lbl_803DE98C:
-	clrlwi.  r0, r0, 0x18
-	beq      lbl_803DE9C8
-	lbz      r0, 0x10(r30)
-	cmplwi   r0, 0
-	bne      lbl_803DEA28
-	mr       r3, r31
-	li       r4, 1
-	bl       setYesNo__Q43ebi6Screen10FileSelect11TMainScreenFb
-	li       r0, 1
-	li       r4, 0x1802
-	stb      r0, 0x10(r30)
-	li       r5, 0
-	lwz      r3, spSysIF__8PSSystem@sda21(r13)
-	bl       playSystemSe__Q28PSSystem5SysIFFUlUl
-	b        lbl_803DEA28
-
-lbl_803DE9C8:
-	lwz      r0, 0x18(r3)
-	rlwinm.  r0, r0, 0, 0x1d, 0x1d
-	bne      lbl_803DE9E4
-	lfs      f1, 0x4c(r3)
-	lfs      f0, lbl_8051FC0C@sda21(r2)
-	fcmpo    cr0, f1, f0
-	bge      lbl_803DE9EC
-
-lbl_803DE9E4:
-	li       r0, 1
-	b        lbl_803DE9F0
-
-lbl_803DE9EC:
-	li       r0, 0
-
-lbl_803DE9F0:
-	clrlwi.  r0, r0, 0x18
-	beq      lbl_803DEA28
-	lbz      r0, 0x10(r30)
-	cmplwi   r0, 1
-	bne      lbl_803DEA28
-	mr       r3, r31
-	li       r4, 0
-	bl       setYesNo__Q43ebi6Screen10FileSelect11TMainScreenFb
-	li       r0, 0
-	li       r4, 0x1802
-	stb      r0, 0x10(r30)
-	li       r5, 0
-	lwz      r3, spSysIF__8PSSystem@sda21(r13)
-	bl       playSystemSe__Q28PSSystem5SysIFFUlUl
-
-lbl_803DEA28:
-	lwz      r3, 0xc00(r31)
-	lwz      r3, 0x1c(r3)
-	rlwinm.  r0, r3, 0, 0x17, 0x17
-	beq      lbl_803DEA54
-	li       r0, 1
-	li       r4, 0x1800
-	stb      r0, 0x21(r30)
-	li       r5, 0
-	lwz      r3, spSysIF__8PSSystem@sda21(r13)
-	bl       playSystemSe__Q28PSSystem5SysIFFUlUl
-	b        lbl_803DEAAC
-
-lbl_803DEA54:
-	rlwinm.  r0, r3, 0, 0x16, 0x16
-	beq      lbl_803DEAAC
-	li       r3, 0
-	li       r0, 1
-	stb      r3, 0x10(r30)
-	li       r4, 0x1801
-	li       r5, 0
-	stb      r0, 0x21(r30)
-	lwz      r3, spSysIF__8PSSystem@sda21(r13)
-	bl       playSystemSe__Q28PSSystem5SysIFFUlUl
-	b        lbl_803DEAAC
-
-lbl_803DEA80:
-	lbz      r0, 0x20(r30)
-	cmplwi   r0, 0
-	bne      lbl_803DEAAC
-	li       r0, 1
-	stb      r0, 0x20(r30)
-	lwz      r12, 0(r3)
-	lwz      r12, 0x2c(r12)
-	mtctr    r12
-	bctrl
-	mr       r3, r31
-	bl       closeMSG__Q43ebi6Screen10FileSelect11TMainScreenFv
-
-lbl_803DEAAC:
-	mr       r3, r31
-	bl       isFinishCloseMSG__Q43ebi6Screen10FileSelect11TMainScreenFv
-	clrlwi.  r0, r3, 0x18
-	beq      lbl_803DEB24
-	lbz      r0, 0x21(r30)
-	cmplwi   r0, 0
-	beq      lbl_803DEB24
-	lbz      r0, 0x20(r30)
-	cmplwi   r0, 0
-	beq      lbl_803DEB24
-	lbz      r0, 0x10(r30)
-	cmplwi   r0, 0
-	beq      lbl_803DEB04
-	mr       r3, r30
-	mr       r4, r31
-	lwz      r12, 0(r30)
-	li       r6, 0
-	lwz      r5, 0x18(r30)
-	lwz      r12, 0x1c(r12)
-	mtctr    r12
-	bctrl
-	b        lbl_803DEB24
-
-lbl_803DEB04:
-	mr       r3, r30
-	mr       r4, r31
-	lwz      r12, 0(r30)
-	li       r6, 0
-	lwz      r5, 0x1c(r30)
-	lwz      r12, 0x1c(r12)
-	mtctr    r12
-	bctrl
-
-lbl_803DEB24:
-	lwz      r0, 0x14(r1)
-	lwz      r31, 0xc(r1)
-	lwz      r30, 8(r1)
-	mtlr     r0
-	addi     r1, r1, 0x10
-	blr
-	*/
 }
 
 /**
@@ -652,6 +489,7 @@ void FSMState00_SelectData::do_init(TMgr* mgr, Game::StateArg*)
  */
 void FSMState00_SelectData::do_exec(TMgr* mgr)
 {
+	bool save;
 	if (mgr->mMainScreen.isWaitScreen()) {
 		if (mCounter) {
 			mCounter--;
@@ -660,12 +498,13 @@ void FSMState00_SelectData::do_exec(TMgr* mgr)
 		int prev            = mgr->mCurrSelection;
 		mgr->mCountNumInterface.update();
 
-		bool save                           = true;
+		save                                = true;
 		Screen::FileSelect::TFileData* data = &mgr->mMainScreen.mFileData[mgr->mCurrSelection];
 		if (data->mIsNewFile && !data->mIsBrokenFile) {
 			save = false;
 		}
 
+		// something dumb here
 		if (mgr->mCurrSelection != prev) {
 			mgr->mMainScreen.outDataBall(prev);
 			mgr->mMainScreen.closeDataWindow();
@@ -710,179 +549,6 @@ void FSMState00_SelectData::do_exec(TMgr* mgr)
 	if (mgr->mMainScreen.isFinishScreen()) {
 		mgr->goEnd_(TMgr::END_Cancel);
 	}
-	/*
-	stwu     r1, -0x20(r1)
-	mflr     r0
-	stw      r0, 0x24(r1)
-	stmw     r27, 0xc(r1)
-	mr       r31, r4
-	mr       r30, r3
-	mr       r3, r31
-	bl       isWaitScreen__Q33ebi6Screen11TScreenBaseFv
-	clrlwi.  r0, r3, 0x18
-	beq      lbl_803DF8D0
-	lwz      r3, 0x14(r30)
-	cmplwi   r3, 0
-	beq      lbl_803DF6F4
-	addi     r0, r3, -1
-	stw      r0, 0x14(r30)
-
-lbl_803DF6F4:
-	lwz      r28, 0xc00(r31)
-	addi     r3, r31, 0xc04
-	lwz      r27, 0xc30(r31)
-	bl       update__Q23ebi24EUTPadInterface_countNumFv
-	lwz      r0, 0xc30(r31)
-	li       r29, 1
-	mulli    r3, r0, 0x34
-	addi     r3, r3, 0xb00
-	add      r3, r31, r3
-	lbz      r0, 1(r3)
-	cmplwi   r0, 0
-	beq      lbl_803DF734
-	lbz      r0, 0(r3)
-	cmplwi   r0, 0
-	bne      lbl_803DF734
-	li       r29, 0
-
-lbl_803DF734:
-	lwz      r0, 0xc30(r31)
-	cmpw     r27, r0
-	beq      lbl_803DF794
-	mr       r3, r31
-	mr       r4, r27
-	bl       outDataBall__Q43ebi6Screen10FileSelect11TMainScreenFl
-	mr       r3, r31
-	bl       closeDataWindow__Q43ebi6Screen10FileSelect11TMainScreenFv
-	lwz      r4, 0xc30(r31)
-	mr       r3, r31
-	bl       inDataBall__Q43ebi6Screen10FileSelect11TMainScreenFl
-	clrlwi.  r0, r29, 0x18
-	beq      lbl_803DF774
-	mr       r3, r31
-	bl       inCopyErase__Q43ebi6Screen10FileSelect11TMainScreenFv
-	b        lbl_803DF77C
-
-lbl_803DF774:
-	mr       r3, r31
-	bl       outCopyErase__Q43ebi6Screen10FileSelect11TMainScreenFv
-
-lbl_803DF77C:
-	li       r0, 1
-	li       r4, 0x1802
-	stb      r0, 0x10(r30)
-	li       r5, 0
-	lwz      r3, spSysIF__8PSSystem@sda21(r13)
-	bl       playSystemSe__Q28PSSystem5SysIFFUlUl
-
-lbl_803DF794:
-	mr       r3, r31
-	bl       isFinishCloseDataWindow__Q43ebi6Screen10FileSelect11TMainScreenFv
-	clrlwi.  r0, r3, 0x18
-	beq      lbl_803DF7C4
-	lbz      r0, 0x10(r30)
-	cmplwi   r0, 0
-	beq      lbl_803DF7C4
-	li       r0, 0
-	mr       r3, r31
-	stb      r0, 0x10(r30)
-	lwz      r4, 0xc30(r31)
-	bl       openDataWindow__Q43ebi6Screen10FileSelect11TMainScreenFl
-
-lbl_803DF7C4:
-	lwz      r0, 0x14(r30)
-	cmplwi   r0, 0
-	bne      lbl_803DF8D0
-	lwz      r3, 0x1c(r28)
-	rlwinm.  r0, r3, 0, 0x17, 0x17
-	beq      lbl_803DF810
-	mr       r3, r30
-	mr       r4, r31
-	lwz      r12, 0(r30)
-	li       r5, 2
-	li       r6, 0
-	lwz      r12, 0x1c(r12)
-	mtctr    r12
-	bctrl
-	lwz      r3, spSysIF__8PSSystem@sda21(r13)
-	li       r4, 0x1800
-	li       r5, 0
-	bl       playSystemSe__Q28PSSystem5SysIFFUlUl
-	b        lbl_803DF8D0
-
-lbl_803DF810:
-	rlwinm.  r0, r3, 0, 0x14, 0x14
-	beq      lbl_803DF85C
-	clrlwi.  r0, r29, 0x18
-	beq      lbl_803DF8D0
-	mr       r3, r30
-	mr       r4, r31
-	lwz      r12, 0(r30)
-	li       r5, 8
-	li       r6, 0
-	lwz      r12, 0x1c(r12)
-	mtctr    r12
-	bctrl
-	mr       r3, r31
-	bl       closeDataWindow__Q43ebi6Screen10FileSelect11TMainScreenFv
-	lwz      r3, spSysIF__8PSSystem@sda21(r13)
-	li       r4, 0x1800
-	li       r5, 0
-	bl       playSystemSe__Q28PSSystem5SysIFFUlUl
-	b        lbl_803DF8D0
-
-lbl_803DF85C:
-	rlwinm.  r0, r3, 0, 0x15, 0x15
-	beq      lbl_803DF8A0
-	clrlwi.  r0, r29, 0x18
-	beq      lbl_803DF8D0
-	mr       r3, r30
-	mr       r4, r31
-	lwz      r12, 0(r30)
-	li       r5, 7
-	li       r6, 0
-	lwz      r12, 0x1c(r12)
-	mtctr    r12
-	bctrl
-	lwz      r3, spSysIF__8PSSystem@sda21(r13)
-	li       r4, 0x1800
-	li       r5, 0
-	bl       playSystemSe__Q28PSSystem5SysIFFUlUl
-	b        lbl_803DF8D0
-
-lbl_803DF8A0:
-	rlwinm.  r0, r3, 0, 0x16, 0x16
-	beq      lbl_803DF8D0
-	mr       r3, r31
-	li       r4, 0
-	lwz      r12, 0(r31)
-	lwz      r12, 0x10(r12)
-	mtctr    r12
-	bctrl
-	lwz      r3, spSysIF__8PSSystem@sda21(r13)
-	li       r4, 0x1801
-	li       r5, 0
-	bl       playSystemSe__Q28PSSystem5SysIFFUlUl
-
-lbl_803DF8D0:
-	mr       r3, r31
-	lwz      r12, 0(r31)
-	lwz      r12, 0x20(r12)
-	mtctr    r12
-	bctrl
-	clrlwi.  r0, r3, 0x18
-	beq      lbl_803DF8F8
-	mr       r3, r31
-	li       r4, 5
-	bl       goEnd___Q33ebi2FS4TMgrFQ43ebi2FS4TMgr7enumEnd
-
-lbl_803DF8F8:
-	lmw      r27, 0xc(r1)
-	lwz      r0, 0x24(r1)
-	mtlr     r0
-	addi     r1, r1, 0x20
-	blr
-	*/
 }
 
 /**
@@ -907,13 +573,13 @@ void FSMState00b_CheckData::do_exec(TMgr* mgr)
 {
 	switch (mStatus) {
 	case CheckDataState_DoLoad:
-		if (sys->mCardMgr->isSaveValid()) {
+		if (sys->mCardMgr->isSaveInvalid()) {
 			P2ASSERTLINE(625, sys->mCardMgr->loadPlayer(mgr->mCurrSelection));
 			mStatus = CheckDataState_GetStatus;
 		}
 		break;
 	case CheckDataState_GetStatus:
-		if (sys->mCardMgr->isSaveValid()) {
+		if (sys->mCardMgr->isSaveInvalid()) {
 			mCardState = sys->mCardMgr->getCardStatus();
 			mStatus    = CheckDataState_Finish;
 		}
