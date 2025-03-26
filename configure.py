26cd94dd4a4baf993caed578fb2e8de381e71c45
HeartPiece <deancsouthwood@gmail.com>
2025-03-27 10:30:08 +1100

    Make `--non-matching` use src files

--- configure.py
+++ configure.py
@@ -141,9 +141,8 @@ config.sjiswrap_path = args.sjiswrap
 config.progress = args.progress
 if not is_windows():
     config.wrapper = args.wrapper
-# Don't build asm unless we're --non-matching
-if not config.non_matching:
-    config.asm_dir = None
+# If you want to use the original EpochFlame-curated .s asm files, comment out this line below
+config.asm_dir = None
 
 # Tool versions
 config.binutils_tag = "2.42-1"
