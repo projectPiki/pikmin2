e68a77b817b22ce2b7f773ffd501246f3b4fb40e
PikHacker <57734583+PikHacker@users.noreply.github.com>
2026-07-23 23:07:23 -0400

    more jsystem fixes (2 links)

--- include/JSystem/J2D/J2DTypes.h
+++ include/JSystem/J2D/J2DTypes.h
@@ -300,7 +300,7 @@ struct J2DTevSwapModeTableInfo {
 	u8 mA; // _03
 };
 
-inline u8 J2DCalcTevSwapTable(u32 r, u32 g, u32 b, u32 a)
+inline u8 J2DCalcTevSwapTable(u8 r, u8 g, u8 b, u8 a)
 {
 	return (r << 6) + (g << 4) + (b << 2) + a;
 }
@@ -314,14 +314,7 @@ struct J2DTevSwapModeTable {
 
 	J2DTevSwapModeTable(const J2DTevSwapModeTableInfo& info) { _00 = J2DCalcTevSwapTable(info.mR, info.mG, info.mB, info.mA); }
 
-	void setTevSwapModeTableInfo(const J2DTevSwapModeTableInfo& info)
-	{
-		u8 r = info.mR;
-		u8 g = info.mG;
-		u8 b = info.mB;
-		u8 a = info.mA;
-		_00  = J2DCalcTevSwapTable(r, g, b, a);
-	}
+	void setTevSwapModeTableInfo(const J2DTevSwapModeTableInfo& info) { _00 = J2DCalcTevSwapTable(info.mR, info.mG, info.mB, info.mA); }
 
 	u8 getR() { return _00 >> 6 & 3; }
 	u8 getG() { return _00 >> 4 & 3; }
--- src/JSystem/J2D/J2DMaterial.cpp
+++ src/JSystem/J2D/J2DMaterial.cpp
@@ -154,15 +154,15 @@ void J2DMaterial::setAnimation(J2DAnmColor* animation)
 {
 	if (animation || mAnmPtr) {
 		makeAnmPointer();
-		mAnmPtr->mColorAnm    = animation;
-		mAnmPtr->mColorIds[0] = -1;
+		mAnmPtr->mColorAnm = animation;
+		mAnmPtr->mColorIds = -1;
 
 		if (animation) {
 			u32 count = animation->getUpdateMaterialNum();
 			u16 index = mIndex;
 			for (u16 i = 0; i < count; i++) {
 				if (index == animation->getUpdateMaterialID(i)) {
-					mAnmPtr->mColorIds[0] = i;
+					mAnmPtr->mColorIds = i;
 					return;
 				}
 			}
@@ -271,9 +271,9 @@ void J2DMaterial::animation()
 		return;
 	}
 
-	if (mAnmPtr->mColorAnm && mAnmPtr->mColorIds[0] != 0xFFFF) {
+	if (mAnmPtr->mColorAnm && mAnmPtr->mColorIds != 0xFFFF) {
 		GXColor color;
-		mAnmPtr->mColorAnm->getColor(mAnmPtr->mColorIds[0], &color);
+		mAnmPtr->mColorAnm->getColor(mAnmPtr->mColorIds, &color);
 		getColorBlock()->setMatColor(0, color);
 	}
 
@@ -315,17 +315,17 @@ void J2DMaterial::animation()
 				ResTIMG* img = mAnmPtr->mPatternAnm->getResTIMG(idx);
 				if (texture->getTexInfo() != img) {
 					JUTPalette* palette = nullptr;
-					u32 tlut            = GX_TLUT0;
+					GXTlut tlut         = GX_TLUT0;
 					if (img->mPaletteFormat != 0) {
 						palette = mAnmPtr->mPatternAnm->getPalette(idx);
 						if (palette->getNumColors() > 256) {
-							tlut = i % 4 + 16;
+							tlut = (GXTlut)(i % 4 + 16);
 						} else {
-							tlut = i;
+							tlut = (GXTlut)i;
 						}
 					}
 
-					texture->storeTIMG(img, palette, (GXTlut)tlut);
+					texture->storeTIMG(img, palette, tlut);
 				}
 			}
 		}
@@ -349,326 +349,4 @@ void J2DMaterial::animation()
 			}
 		}
 	}
-	/*
-	stwu     r1, -0x110(r1)
-	mflr     r0
-	stw      r0, 0x114(r1)
-	stfd     f31, 0x100(r1)
-	psq_st   f31, 264(r1), 0, qr0
-	stfd     f30, 0xf0(r1)
-	psq_st   f30, 248(r1), 0, qr0
-	stfd     f29, 0xe0(r1)
-	psq_st   f29, 232(r1), 0, qr0
-	stfd     f28, 0xd0(r1)
-	psq_st   f28, 216(r1), 0, qr0
-	stfd     f27, 0xc0(r1)
-	psq_st   f27, 200(r1), 0, qr0
-	stmw     r26, 0xa8(r1)
-	mr       r28, r3
-	lwz      r4, 0x84(r3)
-	cmplwi   r4, 0
-	beq      lbl_8005323C
-	lwz      r3, 0(r4)
-	cmplwi   r3, 0
-	beq      lbl_80052ECC
-	lhz      r4, 0x10(r4)
-	cmplwi   r4, 0xffff
-	beq      lbl_80052ECC
-	lwz      r12, 0(r3)
-	addi     r5, r1, 0x20
-	lwz      r12, 0x10(r12)
-	mtctr    r12
-	bctrl
-	lwz      r0, 0x20(r1)
-	stw      r0, 0xc(r1)
-	lbz      r5, 0xc(r1)
-	lbz      r4, 0xd(r1)
-	lbz      r3, 0xe(r1)
-	lbz      r0, 0xf(r1)
-	stb      r5, 0x14(r1)
-	stb      r4, 0x15(r1)
-	stb      r3, 0x16(r1)
-	stb      r0, 0x17(r1)
-	lwz      r0, 0x14(r1)
-	stw      r0, 0x18(r1)
-	lbz      r3, 0x18(r1)
-	lbz      r0, 0x19(r1)
-	stb      r3, 0x10(r28)
-	lbz      r3, 0x1a(r1)
-	stb      r0, 0x11(r28)
-	lbz      r0, 0x1b(r1)
-	stb      r3, 0x12(r28)
-	stb      r0, 0x13(r28)
-
-lbl_80052ECC:
-	lwz      r3, 0x84(r28)
-	lwz      r0, 4(r3)
-	cmplwi   r0, 0
-	beq      lbl_8005300C
-	lis      r3, j2dDefaultTexMtxInfo@ha
-	li       r30, 0
-	addi     r31, r3, j2dDefaultTexMtxInfo@l
-	li       r29, 0
-	b        lbl_80053000
-
-lbl_80052EF0:
-	rlwinm   r3, r30, 1, 0x17, 0x1e
-	lwz      r5, 0x84(r28)
-	addi     r0, r3, 0x12
-	clrlwi   r27, r30, 0x18
-	lhzx     r4, r5, r0
-	cmplwi   r4, 0xffff
-	beq      lbl_80052FF8
-	lwz      r3, 4(r5)
-	addi     r5, r1, 0x34
-	lfs      f1, 8(r3)
-	bl       calcTransform__19J2DAnmTextureSRTKeyCFfUsP17J3DTextureSRTInfo
-	lha      r4, 0x3c(r1)
-	lis      r0, 0x4330
-	lis      r3, j2dDefaultTexMtxInfo@ha
-	lfs      f7, 4(r31)
-	clrlwi   r4, r4, 0x10
-	lfs      f6, 8(r31)
-	stw      r4, 0xa4(r1)
-	addi     r6, r3, j2dDefaultTexMtxInfo@l
-	lfs      f5, 0xc(r31)
-	mr       r4, r27
-	stw      r0, 0xa0(r1)
-	addi     r3, r28, 0x28
-	lfd      f1, lbl_805168D0@sda21(r2)
-	addi     r5, r1, 0x48
-	lfd      f0, 0xa0(r1)
-	lfs      f2, lbl_805168C8@sda21(r2)
-	fsubs    f1, f0, f1
-	lbz      r6, 0(r6)
-	lbz      r0, 1(r31)
-	lfs      f0, lbl_805168CC@sda21(r2)
-	fmuls    f1, f2, f1
-	lfs      f4, 0x10(r31)
-	lfs      f3, 0x14(r31)
-	lfs      f2, 0x18(r31)
-	fdivs    f29, f1, f0
-	lfs      f1, 0x1c(r31)
-	lfs      f0, 0x20(r31)
-	stfs     f7, 0x4c(r1)
-	lfs      f31, 0x34(r1)
-	stfs     f6, 0x50(r1)
-	stfs     f5, 0x54(r1)
-	lfs      f30, 0x38(r1)
-	stb      r6, 0x48(r1)
-	lfs      f28, 0x40(r1)
-	stb      r0, 0x49(r1)
-	lfs      f27, 0x44(r1)
-	stfs     f4, 0x58(r1)
-	stfs     f3, 0x5c(r1)
-	stfs     f2, 0x60(r1)
-	stfs     f1, 0x64(r1)
-	stfs     f0, 0x68(r1)
-	bl       getTexMtx__14J2DTexGenBlockFUlR9J2DTexMtx
-	stfs     f31, 0x58(r1)
-	mr       r4, r27
-	addi     r3, r28, 0x28
-	addi     r5, r1, 0x48
-	stfs     f30, 0x5c(r1)
-	stfs     f29, 0x60(r1)
-	stfs     f28, 0x64(r1)
-	stfs     f27, 0x68(r1)
-	bl       setTexMtx__14J2DTexGenBlockFUlR9J2DTexMtx
-	slwi     r0, r27, 2
-	addi     r4, r29, 0x1e
-	add      r3, r28, r0
-	stb      r4, 0x2e(r3)
-
-lbl_80052FF8:
-	addi     r30, r30, 1
-	addi     r29, r29, 3
-
-lbl_80053000:
-	clrlwi   r0, r30, 0x18
-	cmplwi   r0, 8
-	blt      lbl_80052EF0
-
-lbl_8005300C:
-	lwz      r0, 0x70(r28)
-	cmplwi   r0, 0
-	beq      lbl_80053134
-	lwz      r3, 0x84(r28)
-	lwz      r0, 8(r3)
-	cmplwi   r0, 0
-	beq      lbl_80053134
-	li       r30, 0
-	b        lbl_80053128
-
-lbl_80053030:
-	rlwinm   r29, r30, 1, 0x17, 0x1e
-	lwz      r3, 0x84(r28)
-	addi     r0, r29, 0x22
-	clrlwi   r27, r30, 0x18
-	lhzx     r0, r3, r0
-	cmplwi   r0, 0xffff
-	beq      lbl_80053124
-	lwz      r3, 0x70(r28)
-	mr       r4, r27
-	lwz      r12, 0(r3)
-	lwz      r12, 0xa0(r12)
-	mtctr    r12
-	bctrl
-	or.      r31, r3, r3
-	beq      lbl_80053124
-	lwz      r3, 0x84(r28)
-	addi     r0, r29, 0x22
-	addi     r5, r1, 8
-	lhzx     r26, r3, r0
-	lwz      r3, 8(r3)
-	mr       r4, r26
-	bl       getTexNo__16J2DAnmTexPatternCFUsPUs
-	lwz      r3, 0x70(r28)
-	mr       r4, r27
-	lhz      r5, 8(r1)
-	lwz      r12, 0(r3)
-	lwz      r12, 0x1c(r12)
-	mtctr    r12
-	bctrl
-	lwz      r3, 0x84(r28)
-	mr       r4, r26
-	lwz      r3, 8(r3)
-	bl       getResTIMG__16J2DAnmTexPatternCFUs
-	lwz      r0, 0x20(r31)
-	mr       r29, r3
-	cmplw    r0, r29
-	beq      lbl_80053124
-	lbz      r0, 8(r29)
-	li       r5, 0
-	li       r6, 0
-	cmplwi   r0, 0
-	beq      lbl_80053118
-	lwz      r3, 0x84(r28)
-	mr       r4, r26
-	lwz      r3, 8(r3)
-	bl       getPalette__16J2DAnmTexPatternCFUs
-	lhz      r0, 0x14(r3)
-	mr       r5, r3
-	cmplwi   r0, 0x100
-	ble      lbl_80053114
-	slwi     r0, r30, 0x1e
-	srwi     r3, r27, 0x1f
-	subf     r0, r3, r0
-	rotlwi   r0, r0, 2
-	add      r3, r0, r3
-	addi     r6, r3, 0x10
-	b        lbl_80053118
-
-lbl_80053114:
-	mr       r6, r27
-
-lbl_80053118:
-	mr       r3, r31
-	mr       r4, r29
-	bl       storeTIMG__10JUTTextureFPC7ResTIMGP10JUTPalette7_GXTlut
-
-lbl_80053124:
-	addi     r30, r30, 1
-
-lbl_80053128:
-	clrlwi   r0, r30, 0x18
-	cmplwi   r0, 8
-	blt      lbl_80053030
-
-lbl_80053134:
-	lwz      r0, 0x70(r28)
-	cmplwi   r0, 0
-	beq      lbl_8005323C
-	lwz      r3, 0x84(r28)
-	lwz      r0, 0xc(r3)
-	cmplwi   r0, 0
-	beq      lbl_8005323C
-	li       r26, 0
-	b        lbl_800531C0
-
-lbl_80053158:
-	rlwinm   r3, r26, 1, 0x17, 0x1e
-	lwz      r5, 0x84(r28)
-	addi     r0, r3, 0x32
-	clrlwi   r27, r26, 0x18
-	lhzx     r4, r5, r0
-	cmplwi   r4, 0xffff
-	beq      lbl_800531BC
-	lwz      r3, 0xc(r5)
-	addi     r5, r1, 0x2c
-	bl       getTevColorReg__15J2DAnmTevRegKeyCFUsP11_GXColorS10
-	lha      r7, 0x2c(r1)
-	mr       r4, r27
-	lha      r6, 0x2e(r1)
-	addi     r5, r1, 0x24
-	lha      r3, 0x30(r1)
-	lha      r0, 0x32(r1)
-	sth      r7, 0x24(r1)
-	sth      r6, 0x26(r1)
-	sth      r3, 0x28(r1)
-	sth      r0, 0x2a(r1)
-	lwz      r3, 0x70(r28)
-	lwz      r12, 0(r3)
-	lwz      r12, 0x34(r12)
-	mtctr    r12
-	bctrl
-
-lbl_800531BC:
-	addi     r26, r26, 1
-
-lbl_800531C0:
-	clrlwi   r0, r26, 0x18
-	cmplwi   r0, 4
-	blt      lbl_80053158
-	li       r26, 0
-	b        lbl_80053230
-
-lbl_800531D4:
-	rlwinm   r3, r26, 1, 0x17, 0x1e
-	lwz      r4, 0x84(r28)
-	addi     r0, r3, 0x3a
-	clrlwi   r27, r26, 0x18
-	lhzx     r4, r4, r0
-	cmplwi   r4, 0xffff
-	beq      lbl_8005322C
-	li       r0, -1
-	addi     r5, r1, 0x1c
-	stw      r0, 0x1c(r1)
-	lwz      r3, 0x84(r28)
-	lwz      r3, 0xc(r3)
-	bl       getTevKonstReg__15J2DAnmTevRegKeyCFUsP8_GXColor
-	lwz      r0, 0x1c(r1)
-	mr       r4, r27
-	addi     r5, r1, 0x10
-	stw      r0, 0x10(r1)
-	lwz      r3, 0x70(r28)
-	lwz      r12, 0(r3)
-	lwz      r12, 0x3c(r12)
-	mtctr    r12
-	bctrl
-
-lbl_8005322C:
-	addi     r26, r26, 1
-
-lbl_80053230:
-	clrlwi   r0, r26, 0x18
-	cmplwi   r0, 4
-	blt      lbl_800531D4
-
-lbl_8005323C:
-	psq_l    f31, 264(r1), 0, qr0
-	lfd      f31, 0x100(r1)
-	psq_l    f30, 248(r1), 0, qr0
-	lfd      f30, 0xf0(r1)
-	psq_l    f29, 232(r1), 0, qr0
-	lfd      f29, 0xe0(r1)
-	psq_l    f28, 216(r1), 0, qr0
-	lfd      f28, 0xd0(r1)
-	psq_l    f27, 200(r1), 0, qr0
-	lfd      f27, 0xc0(r1)
-	lmw      r26, 0xa8(r1)
-	lwz      r0, 0x114(r1)
-	mtlr     r0
-	addi     r1, r1, 0x110
-	blr
-	*/
 }
